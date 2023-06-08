b078ed0b50fe5fac7995fb69f8baffd569b43ba1
lorenzo <lorenzotijerina@gmail.com>
2023-06-08 11:39:16 -0400

    update user model

--- tax-service-api/app/models/user.rb
+++ tax-service-api/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
-end
+    has_secure_password
+  
+    validates :email, presence: true, uniqueness: true
+  
+    enum role: { client: 0, tax_professional: 1, admin: 2 }
+  end
+  
