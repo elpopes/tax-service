0532249da3f336f8b002ab4b3a8a9121bd71846f
lorenzo <lorenzotijerina@gmail.com>
2023-07-11 15:46:15 -0400

    create modal

---
+++ tax-client/src/components/SignInModal.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function SignInModal() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleOpen = () => {
+    setIsVisible(true);
+  };
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  return (
+    <div>
+      <button onClick={handleOpen}>Sign In</button>
+      {isVisible && (
+        <div>
+          <h2>Sign In</h2>
+          <form>
+            <label>
+              Username:
+              <input type="text" name="username" />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input type="password" name="password" />
+            </label>
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+          <button onClick={handleClose}>Close</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SignInModal;
