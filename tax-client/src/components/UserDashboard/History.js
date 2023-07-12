c4692b46950e4fb48fc088986bfe4e8604ae9d38
lorenzo <lorenzotijerina@gmail.com>
2023-07-12 09:18:48 -0400

    history

---
+++ tax-client/src/components/UserDashboard/History.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function History() {
+  return (
+    <div>
+      <h2>History</h2>
+      {/* List of previous tax filings goes here */}
+    </div>
+  );
+}
+
+export default History;
