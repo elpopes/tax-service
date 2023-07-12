ed4b4935dd2af0a5e1fd5e46355ebd826a51887d
lorenzo <lorenzotijerina@gmail.com>
2023-07-12 09:19:54 -0400

    create support

---
+++ tax-client/src/components/UserDashboard/Support.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Support() {
+  return (
+    <div>
+      <h2>Support</h2>
+      {/* Support information goes here */}
+    </div>
+  );
+}
+
+export default Support;
