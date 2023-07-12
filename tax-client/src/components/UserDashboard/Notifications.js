16ee3dc87dca1d3ea9625ded4c2d51ef77db2383
lorenzo <lorenzotijerina@gmail.com>
2023-07-12 09:19:19 -0400

    create notifications

---
+++ tax-client/src/components/UserDashboard/Notifications.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notifications() {
+  return (
+    <div>
+      <h2>Notifications</h2>
+      {/* List of notifications goes here */}
+    </div>
+  );
+}
+
+export default Notifications;
