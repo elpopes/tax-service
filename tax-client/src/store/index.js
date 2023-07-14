a27a197b166a15cf2a64efcd813d34eb086ebd5b
lorenzo <lorenzotijerina@gmail.com>
2023-07-14 08:31:12 -0400

    use configureStore

--- tax-client/src/store/index.js
+++ tax-client/src/store/index.js
@@ -1,19 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 
 import App from "./App";
 import rootReducer from "./store/reducers/rootReducer";
 
-// For Redux DevTools
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer, // the combined reducers
-  composeEnhancers(applyMiddleware(thunk)) // applying middleware
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
 
 ReactDOM.render(
   <React.StrictMode>
