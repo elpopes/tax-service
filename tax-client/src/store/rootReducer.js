e0b5df2e6d9a12433589b08fe173d9e05dda64e7
lorenzo <lorenzotijerina@gmail.com>
2023-07-18 11:44:54 -0400

    update root reducer

--- tax-client/src/store/rootReducer.js
+++ tax-client/src/store/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import usersReducer from "./users/usersReducer";
+import householdsReducer from "./households/householdsReducer";
+import dependentsReducer from "./dependents/dependentsReducer";
 
 const rootReducer = combineReducers({
   users: usersReducer,
+  households: householdsReducer,
+  dependents: dependentsReducer,
 });
 
 export default rootReducer;
