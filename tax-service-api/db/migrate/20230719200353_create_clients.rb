563fea60efec522c965f664415f48a3c2d4af4a7
lorenzo <lorenzotijerina@gmail.com>
2023-07-19 16:06:52 -0400

    add client table

--- tax-service-api/db/migrate/20230719200353_create_clients.rb
+++ tax-service-api/db/migrate/20230719200353_create_clients.rb
@@ -1,14 +1,15 @@
 class CreateClients < ActiveRecord::Migration[7.0]
-  def change
-    create_table :clients do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :full_name
-      t.integer :filing_status
-      t.date :dob
-      t.string :ssn
-      t.references :driver_license, null: false, foreign_key: true
-
-      t.timestamps
+    def change
+      create_table :clients do |t|
+        t.references :user, null: false, foreign_key: true
+        t.string :full_name, null: false
+        t.integer :filing_status
+        t.date :dob
+        t.string :ssn
+        t.references :driver_license
+  
+        t.timestamps
+      end
     end
   end
-end
+  
--- tax-service-api/db/schema.rb
+++ tax-service-api/db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_195754) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_200353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "full_name", null: false
+    t.integer "filing_status"
+    t.date "dob"
+    t.string "ssn"
+    t.bigint "driver_license_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["driver_license_id"], name: "index_clients_on_driver_license_id"
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -25,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_195754) do
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
+  add_foreign_key "clients", "users"
 end
