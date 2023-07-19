bcafb64e13968bfdea3a06cb7af45d2dca16e3b2
lorenzo <lorenzotijerina@gmail.com>
2023-07-19 15:56:52 -0400

    update user model

--- tax-service-api/app/models/user.rb
+++ tax-service-api/app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
     enum role: { client: 0, tax_professional: 1, admin: 2 }
   
     # Callbacks
-    after_initialize :ensure_role
+    after_initialize :ensure_role, :ensure_session_token
   
     # Validations
-    validates :email, presence: true, uniqueness: true
+    validates :email, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
   
@@ -36,12 +36,10 @@ class User < ApplicationRecord
     private
   
     def generate_unique_session_token
-      session_token = SecureRandom::urlsafe_base64
+      self.session_token ||= SecureRandom::urlsafe_base64
     end
   
     def ensure_role
       self.role ||= :client
     end
-  end
-  
-  
+end
