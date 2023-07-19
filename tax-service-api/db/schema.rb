a13372383365be2a381834763daf7fa6ccb264b1
lorenzo <lorenzotijerina@gmail.com>
2023-07-19 16:01:39 -0400

    update user schema

--- tax-service-api/db/schema.rb
+++ tax-service-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_08_144636) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_195754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_144636) do
     t.integer "role", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "session_token"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
 end
