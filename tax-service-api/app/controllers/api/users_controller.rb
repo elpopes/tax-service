8f6a56e6a54413732d6970bd0a08b8ed759e6181
lorenzo <lorenzotijerina@gmail.com>
2023-07-20 13:55:15 -0400

    update user controller to remove digest from store

--- tax-service-api/app/controllers/api/users_controller.rb
+++ tax-service-api/app/controllers/api/users_controller.rb
@@ -10,14 +10,16 @@ module Api
   
       # GET /users/:id
       def show
-        render json: @user
+        @user = User.find(params[:id])
+        render_user(@user)
       end
+      
   
       # POST /users
       def create
         @user = User.new(user_params)
         if @user.save
-          render json: @user, status: :created
+          render_user(@user, :created)
         else
           render json: @user.errors, status: :unprocessable_entity
         end
@@ -26,7 +28,7 @@ module Api
       # PATCH/PUT /users/:id
       def update
         if @user.update(user_params)
-          render json: @user
+          render_user(@user)
         else
           render json: @user.errors, status: :unprocessable_entity
         end
@@ -48,6 +50,12 @@ module Api
       def user_params
         params.require(:user).permit(:email, :password, :role)
       end
+
+      def render_user(user, status=:ok)
+        render json: user.as_json(only: [:id, :email, :role]), status: status
+      end
+
     end
+
   end
   
