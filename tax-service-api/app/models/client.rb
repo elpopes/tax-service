6447847760b21e8377c28d7f62af6d6bff841259
lorenzo <lorenzotijerina@gmail.com>
2023-07-20 09:26:49 -0400

    add app level encyrption

--- tax-service-api/app/models/client.rb
+++ tax-service-api/app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ApplicationRecord
+    attr_encrypted :ssn, key: 
     belongs_to :user
   
     enum filing_status: { single: 0, married_joint: 1, married_separate: 2, head_of_household: 3, widower: 4 }
