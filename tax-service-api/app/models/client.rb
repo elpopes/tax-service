07d8226237a58010df0156849a4c598054aeef99
lorenzo <lorenzotijerina@gmail.com>
2023-07-20 09:42:22 -0400

    refactor client model to use key

--- tax-service-api/app/models/client.rb
+++ tax-service-api/app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
-    attr_encrypted :ssn, key: 
+    attr_encrypted :ssn, key: ENV['ENCRYPTION_KEY'] 
     belongs_to :user
   
     enum filing_status: { single: 0, married_joint: 1, married_separate: 2, head_of_household: 3, widower: 4 }
