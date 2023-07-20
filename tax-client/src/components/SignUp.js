e2f040aeb726f353747ddc0e3d3120b1c582cf9e
lorenzo <lorenzotijerina@gmail.com>
2023-07-20 10:28:51 -0400

    refactor signup to create new user

--- tax-client/src/components/SignUp.js
+++ tax-client/src/components/SignUp.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createUser } from "../store/user/actions";
 import Modal from "./Modal";
 
 function SignUp({ isVisible }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const user = { username, password };
+    dispatch(createUser(user));
+  };
+
   return (
     <Modal isVisible={isVisible}>
       <h2>Sign Up</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Username:
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Password:
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <br />
         <input type="submit" value="Submit" />
