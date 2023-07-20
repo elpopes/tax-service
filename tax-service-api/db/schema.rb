528776accd271dee87b0caa21062187a2634585a
lorenzo <lorenzotijerina@gmail.com>
2023-07-20 09:49:16 -0400

    migration for db level encryption

--- tax-service-api/db/schema.rb
+++ tax-service-api/db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_200353) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_20_134732) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
