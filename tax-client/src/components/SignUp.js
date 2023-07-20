b20fd11d7ca3d71692fd6cb012a251b9abac3472
lorenzo <lorenzotijerina@gmail.com>
2023-07-20 10:15:47 -0400

    update signup

--- tax-client/src/components/SignUp.js
+++ tax-client/src/components/SignUp.js
@@ -1,38 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import Modal from "./Modal";
 
-function SignUp() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const handleOpen = () => {
-    setIsVisible(true);
-  };
-
-  const handleClose = () => {
-    setIsVisible(false);
-  };
-
+function SignUp({ isVisible }) {
   return (
-    <div>
-      <button onClick={handleOpen}>Sign Up</button>
-      <Modal isVisible={isVisible}>
-        <h2>Sign Up</h2>
-        <form>
-          <label>
-            Username:
-            <input type="text" name="username" />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input type="password" name="password" />
-          </label>
-          <br />
-          <input type="submit" value="Submit" />
-        </form>
-        <button onClick={handleClose}>Close</button>
-      </Modal>
-    </div>
+    <Modal isVisible={isVisible}>
+      <h2>Sign Up</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </Modal>
   );
 }
 
