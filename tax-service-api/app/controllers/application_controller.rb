5ffe564bfa4a02555c46020f19912d4ad5b782fd
lorenzo <lorenzotijerina@gmail.com>
2023-07-21 10:19:27 -0400

    update AppCntrl for JWT

--- tax-service-api/app/controllers/application_controller.rb
+++ tax-service-api/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JWT.encode(payload, ENV['JWT_SECRET'])
+    end
+
+    def decoded_token(token)
+        begin
+            JWT.decode(token, ENV['JWT_SECRET'], true, algorithm: 'HS256')[0]
+        rescue JWT::ExpiredSignature
+            nil
+        end
+    end
 end
