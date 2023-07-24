c4028cc3b0b64f170bb4cb62954454dde0beeb18
lorenzo <lorenzotijerina@gmail.com>
2023-07-24 10:13:26 -0400

    add error handling to sess reducer

--- tax-client/src/store/sessions/sessionsReducer.js
+++ tax-client/src/store/sessions/sessionsReducer.js
@@ -1,9 +1,14 @@
-import { SESSIONS_SIGN_IN, SESSIONS_SIGN_OUT } from "./sessionsActions";
+import {
+  SESSIONS_SIGN_IN,
+  SESSIONS_SIGN_OUT,
+  SESSIONS_ERROR,
+} from "./sessionsActions";
 
 const initialState = {
   user: null,
   token: null,
   refresh_token: null,
+  error: null,
 };
 
 const sessionsReducer = (state = initialState, action) => {
@@ -17,6 +22,11 @@ const sessionsReducer = (state = initialState, action) => {
       };
     case SESSIONS_SIGN_OUT:
       return initialState;
+    case SESSIONS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
