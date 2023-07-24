a820d1b8d167d1f11c5ddb91df091ad9dd0a2f02
lorenzo <lorenzotijerina@gmail.com>
2023-07-24 10:12:24 -0400

    add error handling to session actions

--- tax-client/src/store/sessions/sessionsActions.js
+++ tax-client/src/store/sessions/sessionsActions.js
@@ -1,6 +1,8 @@
 export const SESSIONS_SIGN_IN = "SESSIONS_SIGN_IN";
 export const SESSIONS_SIGN_OUT = "SESSIONS_SIGN_OUT";
 
+export const SESSIONS_ERROR = "SESSIONS_ERROR";
+
 export const signIn = ({ user, token, refresh_token }) => ({
   type: SESSIONS_SIGN_IN,
   payload: { user, token, refresh_token },
@@ -8,4 +10,9 @@ export const signIn = ({ user, token, refresh_token }) => ({
 
 export const signOut = () => ({
   type: SESSIONS_SIGN_OUT,
+});
+
+export const sessionError = (error) => ({
+  type: SESSIONS_ERROR,
+  payload: error,
 });
