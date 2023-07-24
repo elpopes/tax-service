0c0c77a5f60ec12531bb707ab405f36657338c72
lorenzo <lorenzotijerina@gmail.com>
2023-07-24 10:14:13 -0400

    add sessionsOperations

---
+++ tax-client/src/store/sessions/sessionsOperations.js
@@ -0,0 +1,44 @@
+import { signIn, signOut, sessionError } from "./sessionsActions";
+import config from "../../config";
+
+export const signInUser =
+  ({ email, password }) =>
+  async (dispatch) => {
+    try {
+      const response = await fetch(`${config.API_BASE_URL}/api/sessions`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user: { email, password } }),
+      });
+
+      if (response.ok) {
+        const { user, token, refresh_token } = await response.json();
+        dispatch(signIn({ user, token, refresh_token }));
+      } else {
+        throw new Error("Sign in failed");
+      }
+    } catch (error) {
+      dispatch(sessionError(error.message));
+    }
+  };
+
+export const signOutUser = () => async (dispatch, getState) => {
+  try {
+    const state = getState();
+    const refreshToken = state.sessions.refresh_token;
+
+    const response = await fetch(`${config.API_BASE_URL}/api/sessions`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ refresh_token: refreshToken }),
+    });
+
+    if (response.ok) {
+      dispatch(signOut());
+    } else {
+      throw new Error("Sign out failed");
+    }
+  } catch (error) {
+    dispatch(sessionError(error.message));
+  }
+};
