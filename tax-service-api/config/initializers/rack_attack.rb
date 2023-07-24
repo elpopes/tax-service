974954cfaeb86f26953d754708a5932236315871
lorenzo <lorenzotijerina@gmail.com>
2023-07-24 09:54:42 -0400

    revisit rack attack initializer when setting up load balancer

--- tax-service-api/config/initializers/rack_attack.rb
+++ tax-service-api/config/initializers/rack_attack.rb
@@ -1,4 +1,5 @@
 class Rack::Attack
+    # Revisit when setting up load balancer!!
     Rack::Attack.throttle('req/ip', limit: 5, period: 2.minutes) do |req|
       req.ip if req.path == '/api/sessions/refresh' && req.post?
     end
