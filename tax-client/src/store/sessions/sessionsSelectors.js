78fa18b2b971d528839e35d98b0513a2ddd7e8d3
lorenzo <lorenzotijerina@gmail.com>
2023-07-24 10:09:51 -0400

    add session selectors

---
+++ tax-client/src/store/sessions/sessionsSelectors.js
@@ -0,0 +1,3 @@
+export const selectUser = (state) => state.sessions.user;
+export const selectToken = (state) => state.sessions.token;
+export const selectRefreshToken = (state) => state.sessions.refresh_token;
