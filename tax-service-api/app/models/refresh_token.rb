6d350f18f30c67737722864977ee1fcd8b116d82
lorenzo <lorenzotijerina@gmail.com>
2023-07-25 10:52:08 -0400

    add genater token and set expiration methods to refresh token model

--- tax-service-api/app/models/refresh_token.rb
+++ tax-service-api/app/models/refresh_token.rb
@@ -4,11 +4,21 @@ class RefreshToken < ApplicationRecord
     validates :token, presence: true
     validates :expires_at, presence: true
   
-    before_create :hash_token
+    before_validation :generate_token, :set_expiration_date, on: :create
+    after_validation :hash_token, if: -> { errors.blank? }
   
     private
+  
+    def generate_token
+      self.token = SecureRandom.hex(10)
+    end
+  
+    def set_expiration_date
+      self.expires_at = 1.week.from_now
+    end
   
     def hash_token
       self.token = Digest::SHA256.hexdigest(token)
     end
   end
+  
