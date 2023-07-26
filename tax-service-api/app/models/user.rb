b95c5c65147b5efece363bd8ed80995d71ae6c91
lorenzo <lorenzotijerina@gmail.com>
2023-07-26 14:33:32 -0400

    refactor user model to include Devise modules

--- tax-service-api/app/models/user.rb
+++ tax-service-api/app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     # Enums
     enum role: { client: 0, tax_professional: 1, admin: 2 }
   
+    # Devise modules
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  
     # Associations
     has_many :refresh_tokens
   
@@ -10,29 +13,12 @@ class User < ApplicationRecord
   
     # Validations
     validates :email, presence: true, uniqueness: true
-    validates :password_digest, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
-  
-    # Attributes
-    attr_reader :password
-  
-    def self.find_by_credentials(email, password)
-      user = User.find_by(email: email)
-      user&.is_password?(password) ? user : nil
-    end
-  
-    def is_password?(password)
-      BCrypt::Password.new(self.password_digest).is_password?(password)
-    end
-  
-    def password=(password)
-      @password = password
-      self.password_digest = BCrypt::Password.create(password)
-    end
   
     private
   
     def ensure_role
       self.role ||= :client
     end
-end
+  end
+  
