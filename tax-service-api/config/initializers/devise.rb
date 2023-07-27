3de275f9f5ba86152c1508c607366bcd9c95a907
lorenzo <lorenzotijerina@gmail.com>
2023-07-27 09:29:15 -0400

    add dispatch requests to jwt config

--- tax-service-api/config/initializers/devise.rb
+++ tax-service-api/config/initializers/devise.rb
@@ -155,6 +155,13 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.dispatch_requests = [
+        ['POST', %r{^/login$}]
+      ]
+      jwt.revocation_requests = [
+        ['DELETE', %r{^/logout$}]
+      ]
+      jwt.expiration_time = 1.day.to_i
   end
 
   config.revoke_jwt_on_expiration = true
