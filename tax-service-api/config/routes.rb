f34a9ec94ae06669aefb5d8f56272803331e6163
lorenzo <lorenzotijerina@gmail.com>
2023-07-28 11:24:04 -0400

    refactor routes

--- tax-service-api/config/routes.rb
+++ tax-service-api/config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
     root to: "home#index"
     get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  namespace :api do
-    devise_for :users, controllers: { registrations: 'api/registrations' }
-    post '/sessions', to: 'sessions#create'
-    delete '/sessions/:id', to: 'sessions#revoke'
-    delete '/sessions', to: 'sessions#revoke_all'
-    post '/refresh', to: 'sessions#refresh'
-    resources :users, only: [:index, :show, :create, :update, :destroy]
+    
+    namespace :api do
+      # Registrations, sessions and password management
+      devise_for :users, controllers: { registrations: 'api/registrations' }
+  
+      # Login and logout routes
+      post '/sessions', to: 'sessions#create'
+      delete '/sessions/:id', to: 'sessions#revoke'
+      delete '/sessions', to: 'sessions#revoke_all'
+      post '/refresh', to: 'sessions#refresh'
+  
+      # User management (excluding creation)
+      resources :users, only: [:index, :show, :update, :destroy]
+    end
   end
-end
+  
