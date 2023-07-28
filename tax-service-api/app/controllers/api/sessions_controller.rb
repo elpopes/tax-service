1abb43d1c1d063ba5a541c14a21a37a1c7998903
lorenzo <lorenzotijerina@gmail.com>
2023-07-28 09:35:26 -0400

    refactor sessionsController

--- tax-service-api/app/controllers/api/sessions_controller.rb
+++ tax-service-api/app/controllers/api/sessions_controller.rb
@@ -34,47 +34,47 @@ module Api
       end
   
       # POST /api/sessions/refresh
-    def refresh
-        refresh_token = RefreshToken.find_by(token: params[:refresh_token])
-        if refresh_token&.valid_token?
-            @user = refresh_token.user
-            token = encode_token({user_id: @user.id})
-        
-            # Create new refresh token and invalidate the old one
-            new_refresh_token = @user.refresh_tokens.create!
-            refresh_token.destroy
-        
-            render json: {
-                user: @user.as_json(only: [:id, :email, :role]), 
-                token: token, 
-                refresh_token: new_refresh_token.token
-            }, status: :ok
-        else
-            render json: { error: 'Invalid or expired refresh token' }, status: :unauthorized
+        def refresh
+            refresh_token = RefreshToken.find_by(token: params[:refresh_token])
+            if refresh_token&.valid_token?
+                @user = refresh_token.user
+                token = encode_token({user_id: @user.id})
+            
+                # Create new refresh token and invalidate the old one
+                new_refresh_token = @user.refresh_tokens.create!
+                refresh_token.destroy
+            
+                render json: {
+                    user: @user.as_json(only: [:id, :email, :role]), 
+                    token: token, 
+                    refresh_token: new_refresh_token.token
+                }, status: :ok
+            else
+                render json: { error: 'Invalid or expired refresh token' }, status: :unauthorized
+            end
         end
-    end
 
-    # DELETE /api/sessions/:id
-    def revoke
-        refresh_token = RefreshToken.find_by(token: params[:id])
-        if refresh_token && refresh_token.user == current_user
-          refresh_token.destroy
-          render json: { message: 'Session revoked.' }
-        else
-          render json: { error: 'Invalid session.' }, status: :unauthorized
+        # DELETE /api/sessions/:id
+        def revoke
+            refresh_token = RefreshToken.find_by(token: params[:id])
+            if refresh_token && refresh_token.user == current_user
+            refresh_token.destroy
+            render json: { message: 'Session revoked.' }
+            else
+            render json: { error: 'Invalid session.' }, status: :unauthorized
+            end
+        end
+        
+        # DELETE /api/sessions
+        def revoke_all
+            unless current_user
+            render json: { error: 'Not authenticated' }, status: :unauthorized
+            return
+            end
+        
+            current_user.refresh_tokens.destroy_all
+            render json: { message: 'All sessions revoked.' }
         end
     end
-      
-      # DELETE /api/sessions
-    def revoke_all
-        unless current_user
-          render json: { error: 'Not authenticated' }, status: :unauthorized
-          return
-        end
-      
-        current_user.refresh_tokens.destroy_all
-        render json: { message: 'All sessions revoked.' }
-    end
-  
 end
   
