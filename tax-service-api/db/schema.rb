820f0e5f7fff5bff354220e638dda97f7fc34624
lorenzo <lorenzotijerina@gmail.com>
2023-07-28 10:56:28 -0400

    set role to client by default in table

--- tax-service-api/db/schema.rb
+++ tax-service-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_28_143115) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_28_145531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_28_143115) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "encrypted_password", null: false
-    t.integer "role", default: 0, null: false
+    t.integer "role", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "session_token"
