fca3ad458582552c523494a8d309f6cc1ebe4105
lorenzo <lorenzotijerina@gmail.com>
2023-07-31 15:18:58 -0400

    refactor modal for styling

--- tax-client/src/components/Modal.js
+++ tax-client/src/components/Modal.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./Modal.css";
 
 function Modal({ isVisible, children }) {
   if (!isVisible) {
     return null;
   }
+
   return (
-    <div>
-      <div>{children}</div>
+    <div className="modal-backdrop">
+      <div className="modal-content">{children}</div>
     </div>
   );
 }
