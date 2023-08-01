520cd6e5bcfa1cac59169497c3090ba2b99a0cb2
lorenzo <lorenzotijerina@gmail.com>
2023-08-01 11:32:13 -0400

    update index.js

--- tax-client/src/index.js
+++ tax-client/src/index.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { PersistGate } from "redux-persist/integration/react";
 import "./styles/reset.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./store";
+import { store, persistor } from "./store";
 import { Provider } from "react-redux";
 
 const rootElement = document.getElementById("root");
 createRoot(rootElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
