a07158f57b481eb31040fed9b1505a5585c15fa8
lorenzo <lorenzotijerina@gmail.com>
2023-08-01 12:06:11 -0400

    refactor navbar

--- tax-client/src/components/Navbar.js
+++ tax-client/src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "./Navbar.css";
 import UserDisplay from "./UserDisplay";
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div>
-        <p>Navbar</p>
+      <div className="navbar">
+        <p>Sidney Kahan</p>
+        <div className="contact-info">
+          <p>1153 Beach Channel Dr, Far Rockaway, NY 11691</p>
+          <p>(718) 327-0006</p>
+          <p>sidney.kahan@gmail.com</p>
+        </div>
         <UserDisplay />
       </div>
     );
