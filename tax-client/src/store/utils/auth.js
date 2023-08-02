c6cca7f5e0cc648cfc019ac5514f87f3369309e7
lorenzo <lorenzotijerina@gmail.com>
2023-08-02 14:03:30 -0400

    insall jwt-decode and add auth.js

---
+++ tax-client/src/store/utils/auth.js
@@ -0,0 +1,17 @@
+function isTokenValid() {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const decodedToken = jwtDecode(token);
+
+    // Check if the token has expired
+    return decodedToken.exp * 1000 > Date.now();
+  } catch (e) {
+    console.log("Failed to decode JWT:", e);
+    return false;
+  }
+}
