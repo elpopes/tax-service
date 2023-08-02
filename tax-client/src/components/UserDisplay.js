68b4fd1ebeb87cf007b68d14a072692755fa8102
lorenzo <lorenzotijerina@gmail.com>
2023-08-02 16:41:38 -0400

    refactor userDisplay

--- tax-client/src/components/UserDisplay.js
+++ tax-client/src/components/UserDisplay.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectUser } from "../store/sessions/sessionsSelectors";
+import { selectUser, selectToken } from "../store/sessions/sessionsSelectors";
 import { isTokenValid } from "../store/utils/auth";
 import SignOut from "./SignOut";
 import SignIn from "./SignIn";
 
 function UserDisplay() {
   const user = useSelector(selectUser);
-  const token = useSelector((state) => state.sessions.token);
+  const token = useSelector(selectToken);
 
-  const isUserLoggedIn = user && isTokenValid(token);
+  const isValidToken = token ? isTokenValid(token) : false;
 
   return (
     <div>
-      {isUserLoggedIn ? `Welcome, ${user.email} ` : <SignIn />}
-      {isUserLoggedIn && <SignOut />}
+      {isValidToken && user ? `Welcome, ${user.email} ` : <SignIn />}
+      {isValidToken && user && <SignOut />}
     </div>
   );
 }
