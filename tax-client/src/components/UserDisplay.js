12b910b30a9041a1a03e3ca6f060f431a5326ccb
lorenzo <lorenzotijerina@gmail.com>
2023-08-02 14:07:02 -0400

    refactor UserDisplay to use isTokenValid

--- tax-client/src/components/UserDisplay.js
+++ tax-client/src/components/UserDisplay.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../store/sessions/sessionsSelectors";
+import { isTokenValid } from "../store/utils/auth";
 import SignOut from "./SignOut";
 import SignIn from "./SignIn";
 
 function UserDisplay() {
   const user = useSelector(selectUser);
+  const token = useSelector((state) => state.session.token);
+
+  const isUserLoggedIn = user && isTokenValid(token);
 
   return (
     <div>
-      {user ? `Welcome, ${user.email} ` : <SignIn />}
-      {user && <SignOut />}
+      {isUserLoggedIn ? `Welcome, ${user.email} ` : <SignIn />}
+      {isUserLoggedIn && <SignOut />}
     </div>
   );
 }
