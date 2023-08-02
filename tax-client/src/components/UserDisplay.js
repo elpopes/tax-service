11d412e21b3c9fcac0d8d42242410e4e9ce5d607
lorenzo <lorenzotijerina@gmail.com>
2023-08-02 17:26:29 -0400

    useEffect to signout user

--- tax-client/src/components/UserDisplay.js
+++ tax-client/src/components/UserDisplay.js
@@ -1,15 +1,23 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { selectUser, selectToken } from "../store/sessions/sessionsSelectors";
 import { isTokenValid } from "../store/utils/auth";
 import SignOut from "./SignOut";
 import SignIn from "./SignIn";
+import { signOut } from "../store/sessions/sessionsActions";
 
 function UserDisplay() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const token = useSelector(selectToken);
 
   const isValidToken = token ? isTokenValid(token) : false;
+
+  useEffect(() => {
+    if (!isValidToken && user) {
+      dispatch(signOut());
+    }
+  }, [isValidToken, user, dispatch]);
 
   return (
     <div>
