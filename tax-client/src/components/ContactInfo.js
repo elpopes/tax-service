781a816b4879d5a9464e04350c8e5c2ecf07a9c5
lorenzo <lorenzotijerina@gmail.com>
2023-08-03 12:40:13 -0400

    ContactInfo

---
+++ tax-client/src/components/ContactInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ContactInfo.css";
+
+class ContactInfo extends React.Component {
+  render() {
+    return (
+      <div className="contact-info">
+        <p>1153 Beach Channel Dr, Far Rockaway, NY 11691</p>
+        <p>(718) 327-0006</p>
+        <p>sidney.kahan@gmail.com</p>
+      </div>
+    );
+  }
+}
+
+export default ContactInfo;
