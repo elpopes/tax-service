54cdd515d7fbb77945483af1bde779b51714e18d
lorenzo <lorenzotijerina@gmail.com>
2023-08-03 12:41:54 -0400

    add search and contact in nav

--- tax-client/src/components/Navbar.js
+++ tax-client/src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Navbar.css";
 import UserDisplay from "./UserDisplay";
+import SearchBar from "./SearchBar";
+import ContactInfo from "./ContactInfo";
 
 class Navbar extends React.Component {
   render() {
     return (
       <div className="navbar">
-        <p>Sidney Kahan</p>
-        <div className="contact-info">
-          <p>1153 Beach Channel Dr, Far Rockaway, NY 11691</p>
-          <p>(718) 327-0006</p>
-          <p>sidney.kahan@gmail.com</p>
+        <div className="logo-container">
+          <p>Sidney Kahan</p>
+        </div>
+        <SearchBar />
+        <ContactInfo />
+        <div className="options-container">
+          <p className="option">Support</p>
+          <p className="option">Notifications</p>
         </div>
         <UserDisplay />
       </div>
