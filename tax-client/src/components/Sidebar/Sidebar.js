ac4f0cf32aeae279dd8084f240b136785caa2949
lorenzo <lorenzotijerina@gmail.com>
2023-08-03 14:14:20 -0400

    refactor sidebar.js

--- tax-client/src/components/Sidebar/Sidebar.js
+++ tax-client/src/components/Sidebar/Sidebar.js
@@ -4,10 +4,13 @@ import MyReturns from "./MyReturns";
 import BankProducts from "./BankProducts";
 import MyProperties from "./MyProperties";
 import MyDependents from "./MyDependents";
+import "./Sidebar.css";
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="sidebar">
+      {" "}
+      {/* Add the className here */}
       <h2>Tax Year</h2>
       <select>
         <option value="2023">2023</option>
