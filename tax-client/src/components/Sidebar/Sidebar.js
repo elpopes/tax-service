c190ba2e9ba0e43460268a9b68f98ad958f1d66a
lorenzo <lorenzotijerina@gmail.com>
2023-08-04 09:28:49 -0400

    add react-select

--- tax-client/src/components/Sidebar/Sidebar.js
+++ tax-client/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import UploadFiles from "./UploadFiles";
 import MyReturns from "./MyReturns";
 import BankProducts from "./BankProducts";
@@ -7,15 +7,26 @@ import MyDependents from "./MyDependents";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const date = new Date();
+  const currentYear =
+    date.getMonth() >= 9 ? date.getFullYear() : date.getFullYear() - 1;
+  const [selectedYear, setSelectedYear] = useState(currentYear);
+
+  const years = Array.from({ length: 5 }, (_, i) => currentYear - i);
+
+  const handleChange = (e) => {
+    setSelectedYear(e.target.value);
+  };
+
   return (
     <div className="sidebar">
-      {" "}
-      {/* Add the className here */}
-      <h2>Tax Year</h2>
-      <select>
-        <option value="2023">2023</option>
-        <option value="2022">2022</option>
-        <option value="2021">2021</option>
+      <h2>Tax Year: {selectedYear}</h2>
+      <select value={selectedYear} onChange={handleChange}>
+        {years.map((year) => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
       </select>
       <UploadFiles />
       <MyReturns />
