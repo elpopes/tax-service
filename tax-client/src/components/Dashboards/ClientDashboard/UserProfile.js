7ab26b592a32e044c437403d1079b175e6f800af
lorenzo <lorenzotijerina@gmail.com>
2023-08-07 13:36:23 -0400

    refactor profile component

--- tax-client/src/components/Dashboards/ClientDashboard/UserProfile.js
+++ tax-client/src/components/Dashboards/ClientDashboard/UserProfile.js
@@ -1,12 +1,22 @@
-import React from "react";
-
-const userData = {
-  name: "Lorenzo Tijerina",
-  email: "lorenzotijerina@gmail.com",
-  dateOfBirth: "08-28-1975",
-};
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUserProfile } from "../../../store/users/usersOperations";
 
 function UserProfile() {
+  const dispatch = useDispatch();
+
+  // When the component mounts, fetch the user profile
+  useEffect(() => {
+    dispatch(fetchUserProfile());
+  }, [dispatch]);
+
+  // Retrieve the user data from the Redux store
+  const userData = useSelector((state) => state.users.current); // Adjust this selector based on your Redux state shape
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="user-profile">
       <h2>User Profile</h2>
--- tax-client/src/store/users/usersActions.js
+++ tax-client/src/store/users/usersActions.js
@@ -1,6 +1,12 @@
 export const RECEIVE_USERS = "users/RECEIVE_USERS";
 export const RECEIVE_USER = "users/RECEIVE_USER";
 export const REMOVE_USER = "users/REMOVE_USER";
+export const RECEIVE_USER_PROFILE = "users/RECEIVE_USER_PROFILE";
+
+export const receiveUserProfile = (profile) => ({
+  type: RECEIVE_USER_PROFILE,
+  profile,
+});
 
 export const receiveUsers = (users) => ({
   type: RECEIVE_USERS,
