664e5ef21883d2f5540c93e4ffa3bd3dacd290a6
lorenzo <lorenzotijerina@gmail.com>
2023-08-08 11:56:49 -0400

    update the usersReducer

--- tax-client/src/store/users/usersReducer.js
+++ tax-client/src/store/users/usersReducer.js
@@ -1,15 +1,43 @@
-import { RECEIVE_USERS, RECEIVE_USER, REMOVE_USER } from "./usersActions";
+import {
+  RECEIVE_USERS,
+  RECEIVE_USER,
+  REMOVE_USER,
+  REGISTRATION_ERROR,
+} from "./usersActions";
 
-const usersReducer = (state = {}, action) => {
+const initialState = {
+  users: {},
+  error: null,
+};
+
+const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case RECEIVE_USER:
-      return { ...state, [action.user.id]: action.user };
+      return {
+        ...state,
+        users: {
+          ...state.users,
+          [action.user.id]: action.user,
+        },
+        error: null,
+      };
     case RECEIVE_USERS:
-      return { ...state, ...action.users };
+      return {
+        ...state,
+        users: {
+          ...state.users,
+          ...action.users,
+        },
+      };
     case REMOVE_USER:
       const newState = { ...state };
-      delete newState[action.userId];
+      delete newState.users[action.userId];
       return newState;
+    case REGISTRATION_ERROR:
+      return {
+        ...state,
+        error: action.error,
+      };
     default:
       return state;
   }
