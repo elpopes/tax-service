254865f38fd0aa7e120e3db8a2e02a64cce7a09d
lorenzo <lorenzotijerina@gmail.com>
2023-08-08 11:24:00 -0400

    update user model with ssn encryption

--- tax-service-api/app/models/user.rb
+++ tax-service-api/app/models/user.rb
@@ -1,29 +1,37 @@
 class User < ApplicationRecord
+    # Encryption key from .env
+    ENCRYPTION_KEY = ENV['ATTR_ENCRYPTION_KEY']
+
     # Enums
     enum role: { client: 0, tax_professional: 1, admin: 2 }
-  
+
     # Devise modules
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
-  
+
+    # attr_encrypted setup
+    attr_encrypted :ssn_last_four, key: ENCRYPTION_KEY
+
     # Associations
     has_many :refresh_tokens
     has_one :client
     accepts_nested_attributes_for :client
-  
+
     # Callbacks
     after_initialize :ensure_role
     after_create :create_associated_client
-  
+
     # Validations
     validates :email, presence: true, uniqueness: true
-    validates :password, length: {minimum: 6, allow_nil: true}
-  
+    validates :password, length: { minimum: 6, allow_nil: true }
+    # Add validation for SSN's last four digits length, if it's provided
+    validates :ssn_last_four, length: { is: 4 }, allow_nil: true
+
     private
 
     def create_associated_client
         create_client! if client?
     end
-  
+
     def ensure_role
         self.role ||= :client
     end
