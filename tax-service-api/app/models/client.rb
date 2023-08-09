7b6cd6f33a1e88d2ecc8652be14216acbf00d61a
lorenzo <lorenzotijerina@gmail.com>
2023-08-09 19:47:14 -0400

    update client model

--- tax-service-api/app/models/client.rb
+++ tax-service-api/app/models/client.rb
@@ -1,12 +1,13 @@
 class Client < ApplicationRecord
-    attr_encrypted :ssn, key: ENV['ENCRYPTION_KEY'] 
+    attr_encrypted :ssn, key: ENV['ATTR_ENCRYPTION_KEY'] 
     belongs_to :user
   
     enum filing_status: { single: 0, married_joint: 1, married_separate: 2, head_of_household: 3, widower: 4 }
   
     validates :user, presence: true
     validates :full_name, presence: true
-    validates :ssn, presence: true, uniqueness: true
+    validates :ssn, presence: true
     validates :dob, presence: true
-end
+  end
+  
   
