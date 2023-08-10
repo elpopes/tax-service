b2edbff29e087e35958bef27c3238281ecd6b70c
lorenzo <lorenzotijerina@gmail.com>
2023-08-10 11:04:42 -0400

    refactor creaet associated client

--- tax-service-api/app/models/user.rb
+++ tax-service-api/app/models/user.rb
@@ -30,7 +30,11 @@ class User < ApplicationRecord
 
     def create_associated_client
         return unless client?
-        create_client!
+    
+        client = create_client!
+        unless client.persisted?
+            Rails.logger.info "Client creation failed with errors: #{client.errors.full_messages.join(', ')}"
+        end
     end
 
     def ensure_role
