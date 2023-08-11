015a84903de2c204aab1fffb8514f730054258ae
lorenzo <lorenzotijerina@gmail.com>
2023-08-11 12:07:52 -0400

    clear errors actions

--- tax-client/src/store/sessions/sessionsActions.js
+++ tax-client/src/store/sessions/sessionsActions.js
@@ -1,7 +1,11 @@
 export const SESSIONS_SIGN_IN = "SESSIONS_SIGN_IN";
 export const SESSIONS_SIGN_OUT = "SESSIONS_SIGN_OUT";
-
 export const SESSIONS_ERROR = "SESSIONS_ERROR";
+export const CLEAR_SESSIONS_ERROR = "CLEAR_SESSIONS_ERROR";
+
+export const clearSessionsError = () => ({
+  type: CLEAR_SESSIONS_ERROR,
+});
 
 export const signIn = ({ user, token, refresh_token }) => ({
   type: SESSIONS_SIGN_IN,
