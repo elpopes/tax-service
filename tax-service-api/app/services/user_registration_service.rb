70277ded8d55702eacef8eda0dbf59d87d2dfa0b
lorenzo <lorenzotijerina@gmail.com>
2023-08-16 18:02:00 -0400

    refactor error messages in service object

--- tax-service-api/app/services/user_registration_service.rb
+++ tax-service-api/app/services/user_registration_service.rb
@@ -6,19 +6,25 @@ class UserRegistrationService
     end
   
     def register
-      dob = params[:user][:dob] # Extracting the dob
-      user_params_without_dob = user_params.except(:dob) # Removing dob from user params
-      user = User.new(user_params_without_dob)
+        dob = params[:user][:dob] # Extracting the dob
+        user_params_without_dob = user_params.except(:dob) # Removing dob from user params
       
-      if user.save
-        if user.role == 'client'
-          create_client(user, dob) # Creating a client if the user is a client
+        Rails.logger.info("User Parameters: #{user_params_without_dob}")
+      
+        user = User.new(user_params_without_dob)
+        
+        if user.save
+          if user.role == 'client'
+            create_client(user, dob) # Creating a client if the user is a client
+          end
+          { success: true, user: user }
+        else
+          # Debugging: Print user errors if saving fails
+          Rails.logger.error("User saving failed: #{user.errors.full_messages}")
+          { success: false, errors: user.errors.full_messages }
         end
-        { success: true, user: user }
-      else
-        { success: false, errors: user.errors.full_messages }
-      end
     end
+      
   
     private
   
@@ -30,11 +36,11 @@ class UserRegistrationService
         client_params = {
           user: user,
           dob: dob,
-          ssn: user.ssn_last_four, # Assuming ssn_last_four is available in user, and you want to use it as SSN
+          ssn: user.ssn_last_four,
           first_name: user.first_name,
           last_name: user.last_name,
           middle_name: user.middle_name,
-          ssn_last_four: user.ssn_last_four # Assuming ssn_last_four is available in user
+          ssn_last_four: user.ssn_last_four 
         }
       
         client = Client.new(client_params)
