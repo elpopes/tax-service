5aea5e26d77aad06b4b92a6157a385e63e512be1
lorenzo <lorenzotijerina@gmail.com>
2023-08-16 16:48:00 -0400

    refactore user_reg_srvs

--- tax-service-api/app/services/user_registration_service.rb
+++ tax-service-api/app/services/user_registration_service.rb
@@ -6,8 +6,14 @@ class UserRegistrationService
     end
   
     def register
-      user = User.new(user_params)
+      dob = params[:user][:dob] # Extracting the dob
+      user_params_without_dob = user_params.except(:dob) # Removing dob from user params
+      user = User.new(user_params_without_dob)
+      
       if user.save
+        if user.role == 'client'
+          create_client(user, dob) # Creating a client if the user is a client
+        end
         { success: true, user: user }
       else
         { success: false, errors: user.errors.full_messages }
@@ -17,7 +23,11 @@ class UserRegistrationService
     private
   
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :middle_name, :email, :password, :role, :ssn_last_four)
+      params.require(:user).permit(:first_name, :last_name, :middle_name, :email, :password, :role, :ssn_last_four, :dob)
+    end
+  
+    def create_client(user, dob)
+      # Logic to create a client using the user and dob
     end
   end
   
