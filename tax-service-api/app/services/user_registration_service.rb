39d842ec1d8a6a75366b8a69f79ea14a072fda27
lorenzo <lorenzotijerina@gmail.com>
2023-09-01 10:55:37 -0400

    refactor reg service

--- tax-service-api/app/services/user_registration_service.rb
+++ tax-service-api/app/services/user_registration_service.rb
@@ -6,25 +6,12 @@ class UserRegistrationService
     end
   
     def register
-        dob = params[:user][:dob] # Extracting the dob
-        user_params_without_dob = user_params.except(:dob) # Removing dob from user params
-      
-        Rails.logger.info("User Parameters: #{user_params_without_dob}")
-
-        debugger
-      
-        user = User.new(user_params_without_dob)
-        
-        if user.save
-          if user.role == 'client'
-            create_client(user, dob) # Creating a client if the user is a client
-          end
-          { success: true, user: user }
-        else
-          debugger
-        end
+      user = build_user
+      return { success: false, errors: user.errors.full_messages } unless user.save
+  
+      handle_client_creation(user) if user.client?
+      { success: true, user: user }
     end
-      
   
     private
   
@@ -32,24 +19,29 @@ class UserRegistrationService
       params.require(:user).permit(:first_name, :last_name, :middle_name, :email, :password, :role, :ssn_last_four, :dob)
     end
   
-    def create_client(user, dob)
-        client_params = {
-          user: user,
-          dob: dob,
-          ssn: user.ssn_last_four,
-          first_name: user.first_name,
-          last_name: user.last_name,
-          middle_name: user.middle_name,
-          ssn_last_four: user.ssn_last_four 
-        }
-      
-        debugger
-      
-        unless client.save
-          Rails.logger.error("Client creation failed for user #{user.id}: #{client.errors.full_messages}")
-        end
+    def build_user
+      User.new(user_params.except(:dob))
     end
-      
-      
+  
+    def handle_client_creation(user)
+      client = create_client(user, user_params[:dob])
+      log_client_creation_failure(user, client) unless client.save
+    end
+  
+    def create_client(user, dob)
+      Client.new(
+        user: user,
+        dob: dob,
+        ssn: user.ssn_last_four,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        middle_name: user.middle_name,
+        ssn_last_four: user.ssn_last_four
+      )
+    end
+  
+    def log_client_creation_failure(user, client)
+      Rails.logger.error("Client creation failed for user #{user.id}: #{client.errors.full_messages}")
+    end
   end
   
