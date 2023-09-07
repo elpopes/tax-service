cc9e6eaa541754eb974150d3bbaf2e2059c3b90a
lorenzo <lorenzotijerina@gmail.com>
2023-09-07 13:24:49 -0400

    customize show in confirmcntrl

--- tax-service-api/app/controllers/api/confirmations_controller.rb
+++ tax-service-api/app/controllers/api/confirmations_controller.rb
@@ -1,5 +1,13 @@
 module Api
     class ConfirmationsController < Devise::ConfirmationsController
-      # Custom methods...
+        def show
+            self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+        
+            if resource.errors.empty?
+              redirect_to "http://localhost:5000/email-confirmed"
+            else
+              redirect_to "http://localhost:5000/email-confirmation-failure"
+            end
+        end
     end
   end
