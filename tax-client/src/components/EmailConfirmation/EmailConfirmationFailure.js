1774e0b20862875962a87eefc62a14a10e35f447
lorenzo <lorenzotijerina@gmail.com>
2023-09-07 13:31:33 -0400

    add email confirmation components

---
+++ tax-client/src/components/EmailConfirmation/EmailConfirmationFailure.js
@@ -0,0 +1,4 @@
+const EmailConfirmationFailure = () => {
+  return <h1>There was an issue confirming your email. Please try again.</h1>;
+};
+export default EmailConfirmationFailure;
---
+++ tax-client/src/components/EmailConfirmation/EmailConfirmed.js
@@ -0,0 +1,4 @@
+const EmailConfirmed = () => {
+  return <h1>Your email has been successfully confirmed! Please log in.</h1>;
+};
+export default EmailConfirmed;
