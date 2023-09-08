c85188c384cd109a66be4fa17d4bde41d1b8e5ea
lorenzo <lorenzotijerina@gmail.com>
2023-09-08 12:02:25 -0400

    some button styling

--- tax-client/src/components/Button.js
+++ tax-client/src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button.css";
 
 const Button = ({ onClick, children, ...props }) => (
-  <button onClick={onClick} {...props}>
+  <button className="button" onClick={onClick} {...props}>
     {children}
   </button>
 );
