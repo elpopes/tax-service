3643f55f194f2aab6242bbf487ab24bc72cde106
lorenzo <lorenzotijerina@gmail.com>
2023-09-13 11:28:49 -0400

    add styling

--- tax-client/src/components/ProfilePage.js
+++ tax-client/src/components/ProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { getUser } from "../store/users/usersSelectors";
+import "./ProfilePage.css";
 
 function ProfilePage() {
   const user = useSelector(getUser);
