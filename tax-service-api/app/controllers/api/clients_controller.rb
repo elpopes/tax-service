50497966ac7cff2fae9b08c00b4151d84a692279
lorenzo <lorenzotijerina@gmail.com>
2023-09-20 10:41:51 -0400

    update clients controller

--- tax-service-api/app/controllers/api/clients_controller.rb
+++ tax-service-api/app/controllers/api/clients_controller.rb
@@ -1,31 +1,33 @@
-class ClientsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_client, only: [:update]
-  
-    def update
-        ActiveRecord::Base.transaction do
-          unless @client.update(client_params) && @client.user.update(user_params)
-            raise ActiveRecord::Rollback
-          end
+module Api
+    class ClientsController < ApplicationController
+        before_action :authenticate_user!
+        before_action :set_client, only: [:update]
+    
+        def update
+            ActiveRecord::Base.transaction do
+            unless @client.update(client_params) && @client.user.update(user_params)
+                raise ActiveRecord::Rollback
+            end
+            end
+            render json: @client, status: :ok
+        rescue ActiveRecord::Rollback
+            render json: { errors: @client.errors.full_messages + @client.user.errors.full_messages }, status: :unprocessable_entity
         end
-        render json: @client, status: :ok
-    rescue ActiveRecord::Rollback
-        render json: { errors: @client.errors.full_messages + @client.user.errors.full_messages }, status: :unprocessable_entity
-    end
-      
-  
-    private
-  
-    def set_client
-      @client = current_user.client
-    end
-  
-    def client_params
-      params.require(:client).permit(:filing_status, :dob, :driver_license_id, :first_name, :last_name, :middle_name, :ssn_last_four)
-    end
-  
-    def user_params
-      params.require(:user).permit(:email, :first_name, :last_name, :middle_name)
+        
+    
+        private
+    
+        def set_client
+        @client = current_user.client
+        end
+    
+        def client_params
+        params.require(:client).permit(:filing_status, :dob, :driver_license_id, :first_name, :last_name, :middle_name, :ssn_last_four)
+        end
+    
+        def user_params
+        params.require(:user).permit(:email, :first_name, :last_name, :middle_name)
+        end
     end
 end
-  
+    
--- tax-service-api/app/controllers/api/users_controller.rb
+++ tax-service-api/app/controllers/api/users_controller.rb
@@ -93,5 +93,5 @@ module Api
 
     end
 
-  end
+end
   
