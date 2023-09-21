767b5fd8f0960ee0df6ffc0e5cfd64bd98fe1761
lorenzo <lorenzotijerina@gmail.com>
2023-09-21 11:36:19 -0400

    add check in clients operations

--- tax-client/src/store/clients/clientsOperations.js
+++ tax-client/src/store/clients/clientsOperations.js
@@ -4,7 +4,6 @@ import config from "../../config";
 export const updateClientOperation =
   (clientData) => async (dispatch, getState) => {
     try {
-      // Extract the user token from the Redux store
       const token = getState().sessions.token;
 
       // Make the API call to update the client
