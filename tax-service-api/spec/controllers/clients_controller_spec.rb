1eed16d31a875bdfe445202fbf8d56f5200ae5a7
lorenzo <lorenzotijerina@gmail.com>
2023-09-21 09:23:12 -0400

    create token web service

--- tax-service-api/spec/controllers/clients_controller_spec.rb
+++ tax-service-api/spec/controllers/clients_controller_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe Api::ClientsController, type: :controller do
   let(:user) { create(:user, :with_client) }
 
   before do
-    sign_in user
+    allow_any_instance_of(ApplicationController).to receive(:authenticate_user).and_return(true)
+    token = ApplicationController.new.encode_token({ user_id: user.id }) 
+    request.headers['Authorization'] = "Bearer #{token}"
   end
 
   def json_response
@@ -16,7 +18,6 @@ RSpec.describe Api::ClientsController, type: :controller do
     context 'when the update is successful' do
       it 'updates the client and user attributes' do
         put :update, params: { id: user.client.id, client: { first_name: 'NewName' }, user: { email: 'new_email@example.com' } }
-
 
         expect(response).to have_http_status(:ok)
         expect(json_response['first_name']).to eq('NewName')
@@ -31,6 +32,11 @@ RSpec.describe Api::ClientsController, type: :controller do
         expect(response).to have_http_status(:unprocessable_entity)
         expect(json_response['errors']).to include("First name can't be blank") 
       end
+    end
+
+    # Additional test to check if the token is correctly passed
+    it 'has a valid token in the Authorization header' do
+      expect(request.headers['Authorization']).to eq("Bearer #{encode_token({ user_id: user.id })}")
     end
   end
 end
