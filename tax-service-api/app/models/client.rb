1af78b18c092edd5ae87dcb204d76fce7bdd5642
lorenzo <lorenzotijerina@gmail.com>
2023-09-21 13:09:02 -0400

    client model

--- tax-service-api/app/models/client.rb
+++ tax-service-api/app/models/client.rb
@@ -3,7 +3,7 @@ class Client < ApplicationRecord
     belongs_to :user
     has_one :spouse
     has_many :dependents
-
+  
     after_update :update_user
   
     enum filing_status: { single: 0, married_joint: 1, married_separate: 2, head_of_household: 3, widower: 4 }
@@ -15,14 +15,14 @@ class Client < ApplicationRecord
     def full_name
       [first_name, middle_name, last_name].compact.join(' ')
     end
-
+  
     private
-
+  
     def update_user(
         first_name: firstName,
         last_name: lastName,
         middle_name: middleName
     )
-  end
-  
+    end
+end
   
