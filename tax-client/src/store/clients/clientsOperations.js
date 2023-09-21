6366d7a40866d80c7d017a6edf1b01d44bb173d0
lorenzo <lorenzotijerina@gmail.com>
2023-09-21 11:08:49 -0400

    update reducer and new clientsOperations

---
+++ tax-client/src/store/clients/clientsOperations.js
@@ -0,0 +1,38 @@
+import { updateClient, updateClientError } from "./clientsActions";
+import config from "../../config";
+
+export const updateClientOperation =
+  (clientData) => async (dispatch, getState) => {
+    try {
+      // Extract the user token from the Redux store
+      const token = getState().sessions.token;
+
+      // Make the API call to update the client
+      const response = await fetch(
+        `${config.API_BASE_URL}/api/clients/update`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ client: clientData }),
+        }
+      );
+
+      const json = await response.json();
+
+      // Check if the update was successful
+      if (response.ok) {
+        dispatch(updateClient(json));
+      } else {
+        const errorMessage = json.errors
+          ? json.errors.join(", ")
+          : "Update failed";
+        dispatch(updateClientError(errorMessage));
+      }
+    } catch (error) {
+      console.error("Client update error:", error);
+      dispatch(updateClientError(error.message));
+    }
+  };
--- tax-client/src/store/rootReducer.js
+++ tax-client/src/store/rootReducer.js
@@ -3,12 +3,14 @@ import usersReducer from "./users/usersReducer";
 import householdsReducer from "./households/householdsReducer";
 import dependentsReducer from "./dependents/dependentsReducer";
 import sessionsReducer from "./sessions/sessionsReducer";
+import clientsReducer from "./clients/clientsReducer";
 
 const rootReducer = combineReducers({
   users: usersReducer,
   households: householdsReducer,
   dependents: dependentsReducer,
   sessions: sessionsReducer,
+  clients: clientsReducer,
 });
 
 export default rootReducer;
