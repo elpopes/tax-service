45e728d15d4385f5feb09764fcdd9e0112d5f2c1
lorenzo <lorenzotijerina@gmail.com>
2023-09-26 18:04:37 -0400

    remove ssn from user model

--- tax-service-api/app/models/user.rb
+++ tax-service-api/app/models/user.rb
@@ -1,15 +1,9 @@
 class User < ApplicationRecord
-    # Encryption key from .env
-    ENCRYPTION_KEY = ENV['ATTR_ENCRYPTION_KEY']
-  
     # Enums
     enum role: { client: 0, tax_professional: 1, admin: 2 }
   
     # Devise modules
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
-  
-    # attr_encrypted setup
-    attr_encrypted :ssn_last_four, key: ENV['ATTR_ENCRYPTION_KEY'], attribute: 'encrypted_ssn_last_four'
   
     # Associations
     has_many :refresh_tokens
@@ -23,7 +17,6 @@ class User < ApplicationRecord
     # Validations
     validates :email, presence: true, uniqueness: true
     validates :password, length: { minimum: 6, allow_nil: true }
-    validates :ssn_last_four, length: { is: 4 }, allow_nil: true
   
     private
   
@@ -31,8 +24,7 @@ class User < ApplicationRecord
       client = create_client(
         first_name: self.first_name,
         last_name: self.last_name,
-        middle_name: self.middle_name,
-        ssn_last_four: self.ssn_last_four
+        middle_name: self.middle_name
       )
       
       unless client.persisted?
