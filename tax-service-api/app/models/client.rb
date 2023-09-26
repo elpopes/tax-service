2deeab247e76ec64ca5a40a97a6866102b2bd67a
lorenzo <lorenzotijerina@gmail.com>
2023-09-26 18:18:43 -0400

    refactor client model

--- tax-service-api/app/models/client.rb
+++ tax-service-api/app/models/client.rb
@@ -1,7 +1,7 @@
 class Client < ApplicationRecord
     attr_encrypted :ssn, key: ENV['ATTR_ENCRYPTION_KEY']
     before_save :set_last_four_of_ssn
-    belongs_to :user
+    belongs_to :user, optional: true 
     has_one :spouse
     has_many :dependents
   
@@ -9,9 +9,7 @@ class Client < ApplicationRecord
   
     enum filing_status: { single: 0, married_joint: 1, married_separate: 2, head_of_household: 3, widower: 4 }
   
-    validates :user, presence: true
-    validates :ssn, presence: true, if: -> { ssn.present? }
-    validates :dob, presence: true, if: -> { dob.present? }
+    validates :user, presence: true, if: -> { user.present? }
   
     def full_name
       [first_name, middle_name, last_name].compact.join(' ')
@@ -20,15 +18,17 @@ class Client < ApplicationRecord
     private
   
     def update_user
+      if user
         user.update(
             first_name: self.first_name,
             last_name: self.last_name,
             middle_name: self.middle_name
         )
+      end
     end
-
+  
     def set_last_four_of_ssn
         self.ssn_last_four = ssn&.last(4)
     end
-end
+  end
   
