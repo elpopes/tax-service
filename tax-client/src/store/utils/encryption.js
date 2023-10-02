b3ab329af9e74b991866b94f29667ce903c74565
lorenzo <lorenzotijerina@gmail.com>
2023-10-02 16:08:53 -0400

    refactor encyption.js

--- tax-client/src/store/utils/encryption.js
+++ tax-client/src/store/utils/encryption.js
@@ -34,14 +34,4 @@ function ab2str(buf) {
   return String.fromCharCode.apply(null, new Uint16Array(buf));
 }
 
-// String to ArrayBuffer
-function str2ab(str) {
-  const buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-  const bufView = new Uint16Array(buf);
-  for (let i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
-
 export default encryptWithPublicKey;
