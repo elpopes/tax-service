6669bf4ff9ac67953a4232bf64680e19cff11925
lorenzo <lorenzotijerina@gmail.com>
2023-10-04 09:52:03 -0400

    update client model

--- tax-service-api/app/models/client.rb
+++ tax-service-api/app/models/client.rb
@@ -1,39 +1,38 @@
 class Client < ApplicationRecord
-    attr_encrypted :ssn, key: ENV['ATTR_ENCRYPTION_KEY'], attribute: 'ssn_encrypted'
-    belongs_to :user, optional: true 
+    belongs_to :user, optional: true
     has_one :spouse
     has_many :dependents
   
-    after_update :update_user
-  
+    # Validate presence of iv when encrypted_data is present
+    validates :iv, presence: true, if: -> { encrypted_data.present? }
+    
     enum filing_status: { single: 0, married_joint: 1, married_separate: 2, head_of_household: 3, widower: 4 }
-  
+    
     validates :user, presence: true, if: -> { user.present? }
-
-    def encrypted_ssn_for_frontend
-        return nil unless self.ssn
-        EncryptionService.encrypt(self.ssn)
-    end
-      
-    def decrypt_ssn_from_frontend(encrypted_data)
-        EncryptionService.decrypt(encrypted_data[:iv], encrypted_data[:cipher_text])
+  
+    def decrypt_ssn
+      EncryptionService.decrypt(self.iv, self.encrypted_data)
     end
   
+    def last_four_ssn
+      decrypted_ssn = decrypt_ssn
+      decrypted_ssn[-4..-1] if decrypted_ssn
+    end
+    
     def full_name
       [first_name, middle_name, last_name].compact.join(' ')
     end
-  
+    
     private
-  
+    
     def update_user
       if user
         user.update(
-            first_name: self.first_name,
-            last_name: self.last_name,
-            middle_name: self.middle_name
+          first_name: self.first_name,
+          last_name: self.last_name,
+          middle_name: self.middle_name
         )
       end
     end
-  
-end
+  end
   
