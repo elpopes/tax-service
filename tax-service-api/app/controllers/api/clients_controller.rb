beb612655daab51b61b7b75cfd3535d5a773ea04
lorenzo <lorenzotijerina@gmail.com>
2023-10-04 10:05:32 -0400

    update client controller again

--- tax-service-api/app/controllers/api/clients_controller.rb
+++ tax-service-api/app/controllers/api/clients_controller.rb
@@ -1,61 +1,58 @@
 module Api
     class ClientsController < ApplicationController
-        before_action :authenticate_user, except: [:create]
-        before_action :set_client, only: [:update, :profile]
-
-        def profile
-            unless @client
-              render json: { error: 'The logged-in user does not have an associated client.', action_required: true }, status: :not_found and return
-            end
-            
-            decrypted_ssn = EncryptionService.decrypt(@client.ssn_encrypted)
-
-            render json: {
-                id: current_user.id,
-                client_id: @client.id, 
-                first_name: @client.first_name, 
-                middle_name: @client.middle_name,
-                last_name: @client.last_name,
-                email: @current_user.email,
-                dob: @client.dob ? @client.dob.strftime('%Y-%m-%d') : nil,
-                filing_status: @client.filing_status,
-                driver_license_id: @client.driver_license_id,
-                number_of_dependents: @client.number_of_dependents,
-                last_four_ssn: @client.last_four_ssn
-            }, status: :ok
+      before_action :authenticate_user, except: [:create]
+      before_action :set_client, only: [:update, :profile]
+      
+      def profile
+        unless @client
+          render json: { error: 'The logged-in user does not have an associated client.', action_required: true }, status: :not_found and return
+        end
+  
+        render json: {
+            id: current_user.id,
+            client_id: @client.id, 
+            first_name: @client.first_name, 
+            middle_name: @client.middle_name,
+            last_name: @client.last_name,
+            email: @current_user.email,
+            dob: @client.dob ? @client.dob.strftime('%Y-%m-%d') : nil,
+            filing_status: @client.filing_status,
+            driver_license_id: @client.driver_license_id,
+            number_of_dependents: @client.number_of_dependents,
+            last_four_ssn: @client.last_four_ssn
+        }, status: :ok
+      end
+  
+      def update
+        Rails.logger.debug "Update Action Called. Params: #{params.inspect}"
+        ActiveRecord::Base.transaction do
+          if params[:client][:encrypted_data].present? && params[:client][:iv].present?
+            @client.encrypted_data = params[:client][:encrypted_data]
+            @client.iv = params[:client][:iv]
           end
-    
-        def update
-            Rails.logger.debug "Update Action Called. Params: #{params.inspect}"
-            ActiveRecord::Base.transaction do
-              if params[:client][:encrypted_data].present? && params[:client][:iv].present?
-                @client.encrypted_data = params[:client][:encrypted_data]
-                @client.iv = params[:client][:iv]
-              end
+  
+          unless @client.update(client_params)
+            Rails.logger.debug "Update failed for Client"
+            raise ActiveRecord::Rollback
+          end
+        end
+        Rails.logger.debug "Successfully updated Client and User"
+        render json: @client, status: :ok
+      rescue ActiveRecord::Rollback
+        Rails.logger.debug "Rollback occurred. Errors: #{@client.errors.full_messages}"
+        render json: { errors: @client.errors.full_messages }, status: :unprocessable_entity
+      end
       
-              unless @client.update(client_params)
-                Rails.logger.debug "Update failed for Client"
-                raise ActiveRecord::Rollback
-              end
-            end
-            Rails.logger.debug "Successfully updated Client and User"
-            render json: @client, status: :ok
-        rescue ActiveRecord::Rollback
-            Rails.logger.debug "Rollback occurred. Errors: #{@client.errors.full_messages}"
-            render json: { errors: @client.errors.full_messages }, status: :unprocessable_entity
-        end
-          
-        
-        private
+      private
     
-        def set_client
-            @client = current_user&.client
-        end
+      def set_client
+        @client = current_user&.client
+      end
       
-        def client_params
-            params.require(:client).permit(:first_name, :last_name, :middle_name, :dob, :filing_status, :driver_license_id, :number_of_dependents, :ssn_encrypted)
-        end
-          
+      def client_params
+        params.require(:client).permit(:first_name, :last_name, :middle_name, :dob, :filing_status, :driver_license_id, :number_of_dependents)
+      end
     end
-end
+  end
+  
     
