2ab2debc64d8cbd0e858359f91331a85b396d482
lorenzo <lorenzotijerina@gmail.com>
2023-10-05 16:20:24 -0400

    update decrypt service

--- tax-service-api/app/services/encryption_service.rb
+++ tax-service-api/app/services/encryption_service.rb
@@ -15,21 +15,36 @@ module EncryptionService
     }
   end
 
-  def self.decrypt(cipher_text)
-    decipher = OpenSSL::Cipher.new('aes-256-gcm')
-    decipher.decrypt
+  def self.decrypt(cipher_text_base64)
+    # Step 3: Log the received Base64 cipher text
+    puts "Received Base64 Cipher Text: #{cipher_text_base64}"
     
-    # Convert the base64 encoded key from the environment variable to binary
-    private_key_binary = Base64.decode64(ENV['PRIVATE_KEY_BASE64'])
+    # Decode the base64 cipher text
+    cipher_text = Base64.decode64(cipher_text_base64)
     
-    # Create an RSA object
-    private_key = OpenSSL::PKey::RSA.new(private_key_binary)
+    # Step 2: Log the length of the cipher text
+    puts "Cipher Text Length: #{cipher_text.bytesize * 8} bits"
     
-    # Set the key for decryption
-    decipher.key = private_key.to_s
+    # Decode the private key from Base64 and assume it's in PEM format
+    private_key_pem = Base64.decode64(ENV['PRIVATE_KEY_BASE64'])
+  
+    # Step 4: Log the decoded private key PEM
+    puts "Decoded Private Key PEM: #{private_key_pem}"
     
-    # Decrypt
-    decipher.update(cipher_text) + decipher.final
+    # Create an RSA private key object
+    private_key = OpenSSL::PKey::RSA.new(private_key_pem)
+    
+    begin
+      # Decrypt the cipher text using the RSA private key
+      plain_text = private_key.private_decrypt(cipher_text, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
+    rescue OpenSSL::PKey::RSAError => e
+      # Step 5: Log any OpenSSL errors
+      puts "OpenSSL Error: #{e.message}"
+      raise
+    end
+    
+    # Return the last 4 digits of the SSN
+    plain_text[-4..-1]
   end
   
 end
