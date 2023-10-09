fc9b2f4d23fa89180a32c06edb9b931a88434980
lorenzo <lorenzotijerina@gmail.com>
2023-10-09 17:41:09 -0400

    fix birthday

--- tax-client/src/components/Dashboards/ClientDashboard/UserProfile.js
+++ tax-client/src/components/Dashboards/ClientDashboard/UserProfile.js
@@ -9,9 +9,9 @@ import {
 
 function formatDate(dob) {
   const date = new Date(dob);
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const year = date.getFullYear();
+  const day = date.getUTCDate();
+  const month = date.getUTCMonth() + 1;
+  const year = date.getUTCFullYear();
 
   return `${month}-${day}-${year}`;
 }
