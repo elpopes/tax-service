cd272304972a7bc7906e01e612af3f57b77c92e1
lorenzo <lorenzotijerina@gmail.com>
2023-10-10 10:56:27 -0400

    render current user client

--- tax-service-api/app/controllers/api/clients_controller.rb
+++ tax-service-api/app/controllers/api/clients_controller.rb
@@ -36,7 +36,19 @@ module Api
           end
         end
         Rails.logger.debug "Successfully updated Client and User"
-        render json: @client, status: :ok
+        render json: {
+            id: current_user.id,
+            client_id: @client.id, 
+            first_name: @client.first_name, 
+            middle_name: @client.middle_name,
+            last_name: @client.last_name,
+            email: @current_user.email,
+            dob: @client.dob ? @client.dob.strftime('%Y-%m-%d') : nil,
+            filing_status: @client.filing_status,
+            driver_license_id: @client.driver_license_id,
+            number_of_dependents: @client.number_of_dependents,
+            last_four_ssn: @client.last_four_ssn
+        }, status: :ok
       rescue ActiveRecord::Rollback
         Rails.logger.debug "Rollback occurred. Errors: #{@client.errors.full_messages}"
         render json: { errors: @client.errors.full_messages }, status: :unprocessable_entity
