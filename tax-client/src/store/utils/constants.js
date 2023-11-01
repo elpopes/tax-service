e9cf8819717f722740cf095280bc57a25152d8cf
lorenzo <lorenzotijerina@gmail.com>
2023-11-01 10:10:38 -0400

    add constants utils

---
+++ tax-client/src/store/utils/constants.js
@@ -0,0 +1,7 @@
+export const FILING_STATUS_MAP = {
+  Single: "single",
+  "Married Filing Jointly": "married_joint",
+  "Married Filing Separately": "married_separate",
+  "Head of Household": "head_of_household",
+  "Qualifying Widow(er)": "widower",
+};
