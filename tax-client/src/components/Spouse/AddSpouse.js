0b4e7d60c65c58e242e3d7edd49634c1067d011d
lorenzo <lorenzotijerina@gmail.com>
2023-11-01 10:59:42 -0400

    ssn for add spouse

--- tax-client/src/components/Spouse/AddSpouse.js
+++ tax-client/src/components/Spouse/AddSpouse.js
@@ -4,12 +4,16 @@ import Button from "../Button";
 import Modal from "../Modal";
 import { addSpouseOperation } from "../../store/clients/clientsOperations";
 import { selectSpouseErrors } from "../../store/clients/clientsSelectors";
+import encryptWithPublicKey from "../../store/utils/encryption";
+import { FILING_STATUS_MAP } from "../../store/utils/constants";
 
 const AddSpouse = () => {
   const dispatch = useDispatch();
   const currentUserId = useSelector((state) => state.sessions.user.id);
   const spouseErrors = useSelector(selectSpouseErrors);
 
+  const [actualSSN, setActualSSN] = useState("");
+  const [formattedSSN, setFormattedSSN] = useState("");
   const [spouseData, setSpouseData] = useState({
     first_name: "",
     last_name: "",
@@ -23,7 +27,7 @@ const AddSpouse = () => {
 
   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const handleChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
     setSpouseData({
       ...spouseData,
@@ -31,9 +35,37 @@ const AddSpouse = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSSNChange = (e) => {
+    const rawSSN = e.target.value.replace(/-/g, "");
+    if (rawSSN.length > 9) return;
+    const formatted = [
+      rawSSN.substring(0, 3),
+      rawSSN.substring(3, 5),
+      rawSSN.substring(5, 9),
+    ]
+      .filter(Boolean)
+      .join("-");
+    setActualSSN(rawSSN);
+    setFormattedSSN(formatted);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(addSpouseOperation(currentUserId, spouseData));
+    if (actualSSN.length !== 9) {
+      alert("Please enter a valid 9-digit SSN.");
+      return;
+    }
+    try {
+      const publicKey = sessionStorage.getItem("public_key");
+      const { ssn_encrypted } = await encryptWithPublicKey(
+        actualSSN,
+        publicKey
+      );
+      const updatedSpouseData = { ...spouseData, ssn_encrypted };
+      dispatch(addSpouseOperation(currentUserId, updatedSpouseData));
+    } catch (error) {
+      console.error("There was a problem:", error);
+    }
   };
 
   return (
@@ -41,62 +73,31 @@ const AddSpouse = () => {
       <Button onClick={() => setIsModalVisible(true)}>Add Spouse</Button>
       <Modal isVisible={isModalVisible} title="Add Spouse">
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="first_name"
-            placeholder="First Name"
-            value={spouseData.first_name}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="last_name"
-            placeholder="Last Name"
-            value={spouseData.last_name}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="middle_name"
-            placeholder="Middle Name"
-            value={spouseData.middle_name}
-            onChange={handleChange}
-          />
-          <input
-            type="date"
-            name="dob"
-            placeholder="Date of Birth"
-            value={spouseData.dob}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
+          {/* ... existing input fields ... */}
+
+          <select
             name="filing_status"
-            placeholder="Filing Status"
             value={spouseData.filing_status}
-            onChange={handleChange}
-          />
+            onChange={handleInputChange}
+            required
+          >
+            <option value="">--Please choose an option--</option>
+            <option value={FILING_STATUS_MAP["Married Filing Separately"]}>
+              Married Filing Separately
+            </option>
+            <option value={FILING_STATUS_MAP["Married Filing Jointly"]}>
+              Married Filing Jointly
+            </option>
+          </select>
+
           <input
             type="text"
-            name="driver_license_id"
-            placeholder="Driver License ID"
-            value={spouseData.driver_license_id}
-            onChange={handleChange}
+            name="ssn"
+            placeholder="Social Security Number"
+            value={formattedSSN}
+            onChange={handleSSNChange}
           />
-          <input
-            type="number"
-            name="number_of_dependents"
-            placeholder="Number of Dependents"
-            value={spouseData.number_of_dependents}
-            onChange={handleChange}
-          />
-          <input
-            type="text"
-            name="ssn_encrypted"
-            placeholder="Social Security Number (Encrypted)"
-            value={spouseData.ssn_encrypted}
-            onChange={handleChange}
-          />
+
           <Button type="submit">Add Spouse</Button>
         </form>
         {Array.isArray(spouseErrors) ? (
