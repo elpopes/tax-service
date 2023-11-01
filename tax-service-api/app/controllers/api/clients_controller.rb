ab2b3cffd07744a4888a25e194d6216a4e1e21a5
lorenzo <lorenzotijerina@gmail.com>
2023-11-01 10:15:12 -0400

    fix controller

--- tax-service-api/app/controllers/api/clients_controller.rb
+++ tax-service-api/app/controllers/api/clients_controller.rb
@@ -65,7 +65,7 @@ module Api
             dob: @client.dob ? @client.dob.strftime('%Y-%m-%d') : nil,
             filing_status: @client.filing_status,
             driver_license_id: @client.driver_license_id,
-            spouse_id: @client.spouse_id
+            spouse_id: @client.spouse_id,
             number_of_dependents: @client.number_of_dependents,
             last_four_ssn: @client.last_four_ssn
         }
