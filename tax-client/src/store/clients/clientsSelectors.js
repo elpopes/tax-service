a0dedf6cc3f3dc468748328ed52d6a42b4313fd8
lorenzo <lorenzotijerina@gmail.com>
2023-11-09 11:48:47 -0500

    debugging selectors

--- tax-client/src/store/clients/clientsSelectors.js
+++ tax-client/src/store/clients/clientsSelectors.js
@@ -4,6 +4,9 @@ export const selectClientErrors = (state) => state.clients.errors;
 export const selectAllClients = (state) => Object.values(state.clients.byId);
 export const selectCurrentClient = (state) => state.clients.currentClient;
 export const selectClientStatus = (state) => state.clients.status;
-export const selectSpouseByClientId = (state, clientId) =>
-  state.clients.byId[clientId]?.spouse || null;
+export const selectSpouseByClientId = (state, clientId) => {
+  const spouse = state.clients.byId[clientId]?.spouse || null;
+  console.log(`Spouse for clientId ${clientId}:`, spouse);
+  return spouse;
+};
 export const selectSpouseErrors = (state) => state.clients.spouseErrors;
