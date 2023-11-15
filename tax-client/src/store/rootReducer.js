3824aa5d5fd49758db4f050fcccf622cd06198de
lorenzo <lorenzotijerina@gmail.com>
2023-11-15 18:34:34 -0500

    adjust root reducer again

--- tax-client/src/store/rootReducer.js
+++ tax-client/src/store/rootReducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "redux";
-// import usersReducer from "./users/usersReducer";
+import usersReducer from "./users/usersReducer";
 // import householdsReducer from "./households/householdsReducer";
 // import dependentsReducer from "./dependents/dependentsReducer";
 import sessionsReducer from "./sessions/sessionsReducer";
 import clientsReducer from "./clients/clientsReducer";
 
 const appReducer = combineReducers({
-  //   users: usersReducer,
+  users: usersReducer,
   //   households: householdsReducer,
   //   dependents: dependentsReducer,
   sessions: sessionsReducer,
