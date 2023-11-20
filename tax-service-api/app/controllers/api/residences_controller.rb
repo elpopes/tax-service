cd1c7245412905dd86d0addef4ab230e71d886e4
lorenzo <lorenzotijerina@gmail.com>
2023-11-20 16:04:58 -0500

    residence_controller

---
+++ tax-service-api/app/controllers/api/residences_controller.rb
@@ -0,0 +1,62 @@
+class ResidencesController < ApplicationController
+    before_action :set_client
+    before_action :set_residence, only: [:show, :edit, :update, :destroy]
+  
+    # GET /clients/:client_id/residences
+    def index
+      @residences = @client.residences
+    end
+  
+    # GET /clients/:client_id/residences/:id
+    def show
+    end
+  
+    # GET /clients/:client_id/residences/new
+    def new
+      @residence = @client.residences.build
+    end
+  
+    # POST /clients/:client_id/residences
+    def create
+      @residence = @client.residences.build(residence_params)
+      if @residence.save
+        redirect_to client_residence_path(@client, @residence), notice: 'Residence was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    # GET /clients/:client_id/residences/:id/edit
+    def edit
+    end
+  
+    # PATCH/PUT /clients/:client_id/residences/:id
+    def update
+      if @residence.update(residence_params)
+        redirect_to client_residence_path(@client, @residence), notice: 'Residence was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    # DELETE /clients/:client_id/residences/:id
+    def destroy
+      @residence.destroy
+      redirect_to client_residences_path(@client), notice: 'Residence was successfully destroyed.'
+    end
+  
+    private
+  
+    def set_client
+      @client = Client.find(params[:client_id])
+    end
+  
+    def set_residence
+      @residence = @client.residences.find(params[:id])
+    end
+  
+    def residence_params
+      params.require(:residence).permit(:street_address, :apartment_number, :city, :state, :zip_code, :county, :is_primary_residence, :property_type, :ownership_status)
+    end
+  end
+  
