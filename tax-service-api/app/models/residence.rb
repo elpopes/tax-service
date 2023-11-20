71588dbe831ea935da257035c6d6ae22fe741df6
lorenzo <lorenzotijerina@gmail.com>
2023-11-20 16:02:43 -0500

    add primary residence validation

--- tax-service-api/app/models/residence.rb
+++ tax-service-api/app/models/residence.rb
@@ -1,6 +1,6 @@
 class Residence < ApplicationRecord
     belongs_to :client
-  
+    validate :only_one_primary_residence_per_client
     validates :street_address, :city, :state, :zip_code, :county, presence: true
   
     enum property_type: { condo: 0, apartment: 1, single_family_home: 2, townhouse: 3 }
@@ -11,6 +11,14 @@ class Residence < ApplicationRecord
   
     def rental_property?
       ownership_status == 'rented' || ownership_status == 'leased'
+    end
+
+    private
+    
+    def only_one_primary_residence_per_client
+        if is_primary_residence && Residence.where(client_id: client_id, is_primary_residence: true).exists?
+          errors.add(:is_primary_residence, 'A client can only have one primary residence.')
+        end
     end
   
   end
