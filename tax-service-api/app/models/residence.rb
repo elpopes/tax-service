272be8323777b72372817e490a627ed56a595ae8
lorenzo <lorenzotijerina@gmail.com>
2023-11-20 15:52:00 -0500

    residence model

--- tax-service-api/app/models/residence.rb
+++ tax-service-api/app/models/residence.rb
@@ -1,3 +1,17 @@
 class Residence < ApplicationRecord
-  belongs_to :client
-end
+    belongs_to :client
+  
+    validates :street_address, :city, :state, :zip_code, :county, presence: true
+  
+    enum property_type: { condo: 0, apartment: 1, single_family_home: 2, townhouse: 3 }
+    enum ownership_status: { owned: 0, rented: 1, leased: 2 }
+  
+    scope :primary_residences, -> { where(is_primary_residence: true) }
+    scope :by_state, -> (state) { where(state: state) }
+  
+    def rental_property?
+      ownership_status == 'rented' || ownership_status == 'leased'
+    end
+  
+  end
+  
