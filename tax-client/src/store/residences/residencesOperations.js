9c6d5ccf36d080f0cfe9d3db95d11f6b6ea0f53f
lorenzo <lorenzotijerina@gmail.com>
2023-11-21 11:44:50 -0500

    start res opertaions

---
+++ tax-client/src/store/residences/residencesOperations.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+import {
+  CREATE_RESIDENCE,
+  UPDATE_RESIDENCE,
+  DELETE_RESIDENCE,
+  FETCH_RESIDENCE,
+  CREATE_RESIDENCE_ERROR,
+  UPDATE_RESIDENCE_ERROR,
+  DELETE_RESIDENCE_ERROR,
+  FETCH_RESIDENCE_ERROR,
+} from "./residencesActions";
+
+// Operation to create a residence
+export const createResidence =
+  (clientId, residenceData) => async (dispatch) => {
+    try {
+      const response = await axios.post(
+        `/api/clients/${clientId}/residences`,
+        residenceData
+      );
+      dispatch({
+        type: CREATE_RESIDENCE,
+        payload: { clientId, residence: response.data },
+      });
+    } catch (error) {
+      dispatch({ type: CREATE_RESIDENCE_ERROR, payload: error.message });
+    }
+  };
+
+// Operation to fetch the primary residence
+export const fetchPrimaryResidence = (clientId) => async (dispatch) => {
+  try {
+    const response = await axios.get(`/api/clients/${clientId}/residences`);
+    // Assuming the API returns the primary residence as the first item in the response array
+    const primaryResidence = response.data[0];
+    dispatch({
+      type: FETCH_RESIDENCE,
+      payload: { clientId, residence: primaryResidence },
+    });
+  } catch (error) {
+    dispatch({ type: FETCH_RESIDENCE_ERROR, payload: error.message });
+  }
+};
+
+// Operation to update the primary residence
+export const updatePrimaryResidence =
+  (clientId, residenceId, residenceData) => async (dispatch) => {
+    try {
+      const response = await axios.put(
+        `/api/clients/${clientId}/residences/${residenceId}`,
+        residenceData
+      );
+      dispatch({
+        type: UPDATE_RESIDENCE,
+        payload: { clientId, residence: response.data },
+      });
+    } catch (error) {
+      dispatch({ type: UPDATE_RESIDENCE_ERROR, payload: error.message });
+    }
+  };
+
+// Operation to delete a residence
+export const deleteResidence = (clientId, residenceId) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/clients/${clientId}/residences/${residenceId}`);
+    dispatch({ type: DELETE_RESIDENCE, payload: { clientId, residenceId } });
+  } catch (error) {
+    dispatch({ type: DELETE_RESIDENCE_ERROR, payload: error.message });
+  }
+};
