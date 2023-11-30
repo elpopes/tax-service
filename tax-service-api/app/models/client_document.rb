29b473ebe2ea34a04ba0d3408304a7d05671925c
lorenzo <lorenzotijerina@gmail.com>
2023-11-29 21:16:01 -0500

    create docs model

--- tax-service-api/app/models/client_document.rb
+++ tax-service-api/app/models/client_document.rb
@@ -1,3 +1,9 @@
 class ClientDocument < ApplicationRecord
-  belongs_to :client
-end
+    belongs_to :client
+  
+    enum document_type: { id: 0, w2: 1, expenses: 2, ss_card: 3, stock_earnings_1099b: 4, crypto_earnings: 5, other: 6 }
+    enum status: { uploaded: 0, processed: 1, failed: 2 }
+  
+    # Add validations and methods
+  end
+  
