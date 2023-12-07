95628ceb3e0598d85e8ce3ba12141c82b273d896
lorenzo <lorenzotijerina@gmail.com>
2023-12-07 14:01:58 -0500

    add selectors

---
+++ tax-client/src/store/documents/documentsSelectors.js
@@ -0,0 +1,3 @@
+export const selectUploading = (state) => state.documents.uploading;
+export const selectDocument = (state) => state.documents.document;
+export const selectUploadError = (state) => state.documents.error;
