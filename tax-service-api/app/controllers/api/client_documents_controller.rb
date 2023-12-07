e464e8e4b01ae9b35d8e395984be761fffbfb15b
lorenzo <lorenzotijerina@gmail.com>
2023-12-07 11:14:32 -0500

    create doc controller

---
+++ tax-service-api/app/controllers/api/client_documents_controller.rb
@@ -0,0 +1,28 @@
+module Api
+    class ClientDocumentsController < ApplicationController
+      before_action :authenticate_client!
+  
+      def create
+        @client_document = current_client.client_documents.new(client_document_params)
+  
+        if @client_document.save
+          if @client_document.upload_to_s3(params[:document])
+            @client_document.update(status: :processed)
+            render json: { message: "Document uploaded and processed successfully" }, status: :ok
+          else
+            @client_document.update(status: :failed)
+            render json: { message: "Failed to upload document" }, status: :unprocessable_entity
+          end
+        else
+          render json: @client_document.errors, status: :unprocessable_entity
+        end
+      end
+  
+      private
+  
+      def client_document_params
+        params.require(:client_document).permit(:file_name, :document_type, :tax_year)
+      end
+    end
+  end
+  
