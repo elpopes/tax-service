f60bce63837d3cd3ac50f6cb463ad62e76d9363d
lorenzo <lorenzotijerina@gmail.com>
2023-12-07 15:18:13 -0500

    update client doc controller

--- tax-service-api/app/controllers/api/client_documents_controller.rb
+++ tax-service-api/app/controllers/api/client_documents_controller.rb
@@ -1,8 +1,10 @@
 module Api
     class ClientDocumentsController < ApplicationController
+      before_action :authenticate_user
+      before_action :set_client
   
       def create
-        @client_document = current_client.client_documents.new(client_document_params)
+        @client_document = @client.client_documents.new(client_document_params)
   
         if @client_document.save
           if @client_document.upload_to_s3(params[:document])
@@ -21,6 +23,10 @@ module Api
   
       def client_document_params
         params.require(:client_document).permit(:file_name, :document_type, :tax_year)
+      end
+  
+      def set_client
+        @client = Client.find(params[:clientId])
       end
     end
   end
