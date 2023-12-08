23f505c994d47b3dcc75089bdc366f0884a5351d
lorenzo <lorenzotijerina@gmail.com>
2023-12-08 12:11:23 -0500

    update credentials

--- tax-service-api/app/controllers/api/client_documents_controller.rb
+++ tax-service-api/app/controllers/api/client_documents_controller.rb
@@ -5,10 +5,11 @@ module Api
   
       def create
         @client_document = @client.client_documents.new(client_document_params)
+    
         @client_document.document.attach(params[:client_document][:document])
-  
+    
         if @client_document.save
-          if @client_document.upload_to_s3(params[:document])
+          if @client_document.upload_to_s3  # No argument should be passed here
             @client_document.update(status: :processed)
             render json: { message: "Document uploaded and processed successfully" }, status: :ok
           else
--- tax-service-api/app/models/client_document.rb
+++ tax-service-api/app/models/client_document.rb
@@ -7,49 +7,50 @@ class ClientDocument < ApplicationRecord
   enum document_type: { id: 0, w2: 1, expenses: 2, ss_card: 3, stock_earnings_1099b: 4, crypto_earnings: 5, other: 6 }
   enum status: { uploaded: 0, processed: 1, failed: 2 }
 
-  def upload_to_s3(uploaded_file)
-    if uploaded_file
-      # Encode the document in Base64
-      encoded_document = Base64.encode64(uploaded_file.read)
-      
+  def upload_to_s3
+    if document.attached?
+      # Read the document directly from Active Storage
+      encoded_document = Base64.encode64(document.download)
+
       # Generate a unique key for the document
-      document_key = "#{SecureRandom.uuid}-#{file_name}"
+      document_key = "#{SecureRandom.uuid}-#{document.filename.to_s}"
 
       # Setup AWS Lambda client
-      lambda_client = Aws::Lambda::Client.new(region: 'us-east-1')
+      lambda_client = Aws::Lambda::Client.new(
+        region: 'us-east-1',
+        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      )
 
       # Prepare the payload for Lambda invocation
       payload = {
-          documentData: encoded_document,
-          documentKey: document_key
+        documentData: encoded_document,
+        documentKey: document_key
       }.to_json
 
       # Invoke the Lambda function
       response = lambda_client.invoke({
-          function_name: 'TaxServiceLambda-putItemFunction-TIiOEHgUxZjz',
-          invocation_type: 'RequestResponse',
-          payload: payload
+        function_name: 'TaxServiceLambda-putItemFunction-TIiOEHgUxZjz',
+        invocation_type: 'RequestResponse',
+        payload: payload
       })
 
       # Interpret the response
       if response.status_code == 200
-          # Update the file_path with the S3 object key
-          update(file_path: document_key)
-          true
+        update(file_path: document_key)
+        true
       else
-          Rails.logger.error("Error uploading document to S3: #{response.error}")
-          false
+        Rails.logger.error("Error uploading document to S3: #{response.error}")
+        false
       end
     else
       Rails.logger.error("No document file was uploaded")
       false
     end
   rescue Aws::Lambda::Errors::ServiceError => e
-    # Log any AWS Lambda errors
     Rails.logger.error("AWS Lambda Service Error: #{e.message}")
     false
   rescue => e
-    # Log any other errors
     Rails.logger.error("Exception in upload_to_s3: #{e.message}")
     false
   end
