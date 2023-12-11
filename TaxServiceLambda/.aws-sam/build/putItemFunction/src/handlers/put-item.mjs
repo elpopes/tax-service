15756d5cfa2d65fa59e64fd043ff2cf75ddf7646
lorenzo <lorenzotijerina@gmail.com>
2023-12-11 14:02:37 -0500

    update lambda fordirect invocation

--- TaxServiceLambda/.aws-sam/build/putItemFunction/src/handlers/put-item.mjs
+++ TaxServiceLambda/.aws-sam/build/putItemFunction/src/handlers/put-item.mjs
@@ -3,35 +3,16 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({ region: process.env.AWS_REGION });
 
 export const putItemHandler = async (event) => {
-  if (event.httpMethod && event.httpMethod !== "POST") {
-    return {
-      statusCode: 405,
-      body: JSON.stringify({ message: "Method Not Allowed" }),
-    };
-  }
-
   try {
-    let documentData, documentKey;
+    const documentData = event.documentData;
+    const documentKey = event.documentKey;
 
-    if (!event.body) {
-      console.error("Event body is undefined");
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ message: "Bad request - Missing event body" }),
-      };
-    }
-    // Safely parsing the event body
-    try {
-      const parsedBody = JSON.parse(event.body);
-      documentData = parsedBody.documentData;
-      documentKey = parsedBody.documentKey;
-    } catch (parseError) {
-      console.error("Error parsing event body:", event.body);
+    if (!documentData || !documentKey) {
+      console.error("Missing documentData or documentKey in event");
       return {
         statusCode: 400,
         body: JSON.stringify({
-          message: "Bad request - Invalid JSON",
-          error: parseError.message,
+          message: "Bad request - Missing documentData or documentKey",
         }),
       };
     }
@@ -43,7 +24,6 @@ export const putItemHandler = async (event) => {
       Bucket: process.env.S3_BUCKET_NAME,
       Key: documentKey,
       Body: decodedDocument,
-      // ContentType removed to allow S3 to infer it
     };
 
     await s3Client.send(new PutObjectCommand(params));
