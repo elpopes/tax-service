c4a2304798ea6a7b0c0f7c19a125bc4cd2bce6bc
lorenzo <lorenzotijerina@gmail.com>
2023-12-12 13:43:56 -0500

    update client model to call read and destroy

--- tax-service-api/app/models/client_document.rb
+++ tax-service-api/app/models/client_document.rb
@@ -8,53 +8,72 @@ class ClientDocument < ApplicationRecord
   enum status: { uploaded: 0, processed: 1, failed: 2 }
 
   def upload_to_s3(encoded_document)
-    # Generate a unique key for the document
     document_key = "#{SecureRandom.uuid}-#{file_name}"
+    payload = { documentData: encoded_document, documentKey: document_key }.to_json
 
-    # Setup AWS Lambda client
-    lambda_client = Aws::Lambda::Client.new(
+    response = invoke_lambda('TaxServiceLambda-putItemFunction-TIiOEHgUxZjz', payload)
+
+    process_lambda_response(response) do
+      update(file_path: document_key)
+    end
+  end
+
+  def get_document_by_id
+    payload = { documentKey: file_path }.to_json
+    response = invoke_lambda('TaxServiceLambda-getItemFunction-GmOvZ5AvvDRQ', payload)
+
+    process_lambda_response(response) do
+      # Handle the retrieved document 
+    end
+  end
+
+  def delete_document
+    payload = { documentKey: file_path }.to_json
+    response = invoke_lambda('TaxServiceLambda-deleteItemFunction-Ruqf1mn50ZK9', payload)
+
+    process_lambda_response(response)
+  end
+
+  private
+
+  def setup_lambda_client
+    Aws::Lambda::Client.new(
       region: 'us-east-1',
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     )
+  end
 
-    # Prepare the payload for Lambda invocation
-    payload = {
-      documentData: encoded_document,
-      documentKey: document_key
-    }.to_json
+  def invoke_lambda(function_name, payload)
+    lambda_client = setup_lambda_client
+    Rails.logger.info("Invoking Lambda function: #{function_name} with payload: #{payload}")
 
-    Rails.logger.info("Sending payload to Lambda: #{payload}")
-    # Invoke the Lambda function
-    response = lambda_client.invoke({
-      function_name: 'TaxServiceLambda-putItemFunction-TIiOEHgUxZjz',
+    lambda_client.invoke({
+      function_name: function_name,
       invocation_type: 'RequestResponse',
       payload: payload
     })
+  rescue Aws::Lambda::Errors::ServiceError => e
+    Rails.logger.error("AWS Lambda Service Error: #{e.message}")
+    nil
+  rescue => e
+    Rails.logger.error("Exception in Lambda invocation: #{e.message}")
+    nil
+  end
 
-    # Interpret the response
-    if response.status_code == 200
-        update(file_path: document_key)
-        true
+  def process_lambda_response(response)
+    if response&.status_code == 200
+      yield if block_given?
+      true
     else
-        # Log detailed information when an error occurs
-        log_lambda_error(response)
-        false
-    end
-    rescue Aws::Lambda::Errors::ServiceError => e
-      Rails.logger.error("AWS Lambda Service Error: #{e.message}")
-      false
-    rescue => e
-      Rails.logger.error("Exception in upload_to_s3: #{e.message}")
+      log_lambda_error(response)
       false
     end
-  
-    private
-  
-    def log_lambda_error(response)
-      Rails.logger.error("Error uploading document to S3. Response status: #{response.status_code}")
-      Rails.logger.error("Response payload: #{response.payload.read}")
-      Rails.logger.error("Complete response: #{response.to_h}")
-    end
-end
+  end
 
+  def log_lambda_error(response)
+    Rails.logger.error("Error interacting with Lambda. Response status: #{response&.status_code}")
+    Rails.logger.error("Response payload: #{response&.payload&.read}")
+    Rails.logger.error("Complete response: #{response&.to_h}")
+  end
+end
