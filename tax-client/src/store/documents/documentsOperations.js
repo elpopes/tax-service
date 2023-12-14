d598ff364ce5a620ed07513c226a33b6f1059cce
lorenzo <lorenzotijerina@gmail.com>
2023-12-14 13:01:45 -0500

    update docOps

--- tax-client/src/store/documents/documentsOperations.js
+++ tax-client/src/store/documents/documentsOperations.js
@@ -3,6 +3,12 @@ import {
   uploadDocumentRequest,
   uploadDocumentSuccess,
   uploadDocumentFailure,
+  fetchDocumentRequest,
+  fetchDocumentSuccess,
+  fetchDocumentFailure,
+  deleteDocumentRequest,
+  deleteDocumentSuccess,
+  deleteDocumentFailure,
 } from "./documentsActions";
 
 export const uploadDocument = (documentData, clientId) => {
@@ -34,3 +40,51 @@ export const uploadDocument = (documentData, clientId) => {
     }
   };
 };
+
+export const fetchDocument =
+  (clientId, documentId) => async (dispatch, getState) => {
+    dispatch(fetchDocumentRequest());
+    const token = getState().sessions.token;
+
+    try {
+      const response = await fetch(
+        `${config.API_BASE_URL}/clients/${clientId}/client_documents/${documentId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) throw new Error("Error fetching document");
+
+      const responseData = await response.json();
+      dispatch(fetchDocumentSuccess(responseData));
+    } catch (error) {
+      dispatch(fetchDocumentFailure(error.toString()));
+    }
+  };
+
+export const deleteDocument =
+  (clientId, documentId) => async (dispatch, getState) => {
+    dispatch(deleteDocumentRequest());
+    const token = getState().sessions.token;
+
+    try {
+      const response = await fetch(
+        `${config.API_BASE_URL}/clients/${clientId}/client_documents/${documentId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) throw new Error("Error deleting document");
+
+      dispatch(deleteDocumentSuccess(documentId));
+    } catch (error) {
+      dispatch(deleteDocumentFailure(error.toString()));
+    }
+  };
