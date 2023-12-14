0db5337b1c69b2f33e7a087dd584ce90477c514b
lorenzo <lorenzotijerina@gmail.com>
2023-12-14 13:06:07 -0500

    update doc selectors

--- tax-client/src/store/documents/documentsSelectors.js
+++ tax-client/src/store/documents/documentsSelectors.js
@@ -1,3 +1,5 @@
 export const selectUploading = (state) => state.documents.uploading;
 export const selectDocument = (state) => state.documents.document;
 export const selectUploadError = (state) => state.documents.error;
+export const selectFetching = (state) => state.documents.fetching;
+export const selectDeleting = (state) => state.documents.deleting;
