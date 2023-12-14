75260c9b411af497e9fa79e5c20df0aa3c1e51a1
lorenzo <lorenzotijerina@gmail.com>
2023-12-14 13:05:03 -0500

    update docReducer

--- tax-client/src/store/documents/documentsReducer.js
+++ tax-client/src/store/documents/documentsReducer.js
@@ -2,6 +2,12 @@ import {
   UPLOAD_DOCUMENT_REQUEST,
   UPLOAD_DOCUMENT_SUCCESS,
   UPLOAD_DOCUMENT_FAILURE,
+  FETCH_DOCUMENT_REQUEST,
+  FETCH_DOCUMENT_SUCCESS,
+  FETCH_DOCUMENT_FAILURE,
+  DELETE_DOCUMENT_REQUEST,
+  DELETE_DOCUMENT_SUCCESS,
+  DELETE_DOCUMENT_FAILURE,
 } from "./documentsActions";
 
 const initialState = {
@@ -30,6 +36,19 @@ export const documentsReducer = (state = initialState, action) => {
         uploading: false,
         error: action.payload,
       };
+    case FETCH_DOCUMENT_REQUEST:
+      return { ...state, fetching: true, error: null };
+    case FETCH_DOCUMENT_SUCCESS:
+      return { ...state, document: action.payload, fetching: false };
+    case FETCH_DOCUMENT_FAILURE:
+      return { ...state, fetching: false, error: action.payload };
+
+    case DELETE_DOCUMENT_REQUEST:
+      return { ...state, deleting: true, error: null };
+    case DELETE_DOCUMENT_SUCCESS:
+      return { ...state, document: null, deleting: false };
+    case DELETE_DOCUMENT_FAILURE:
+      return { ...state, deleting: false, error: action.payload };
     default:
       return state;
   }
