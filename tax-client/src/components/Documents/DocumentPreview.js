4e2b7ca44564f06fba08c4f0099f4d33766748bc
elpopes <lorenzotijerina@gmail.com>
2024-06-17 17:32:13 -0400

    add pdf viewer to DocPreview

--- tax-client/src/components/Documents/DocumentPreview.js
+++ tax-client/src/components/Documents/DocumentPreview.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+const pdfjsVersion = "2.16.105";
 
 const DocumentPreview = ({ file, base64Data }) => {
+  const pdfFile = `data:${file.type};base64,${base64Data}`;
+
   return (
     <div className="file-preview">
       <p>Selected File: {file.name}</p>
       {file.type.startsWith("image/") ? (
-        <img
-          src={`data:${file.type};base64,${base64Data}`}
-          alt="Document Preview"
-          className="preview-image"
-        />
+        <img src={pdfFile} alt="Document Preview" className="preview-image" />
+      ) : file.type === "application/pdf" ? (
+        <div className="preview-pdf">
+          <Worker
+            workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}
+          >
+            <Viewer fileUrl={pdfFile} />
+          </Worker>
+        </div>
       ) : (
         <p>Preview not available for this file type.</p>
       )}
