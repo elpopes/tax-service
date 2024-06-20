08b7bfde256ab46183491335048a3a460a67f162
elpopes <lorenzotijerina@gmail.com>
2024-06-20 12:14:20 -0400

    adjustments to hero

--- tax-client/src/components/LandingPage/HeroBanner.js
+++ tax-client/src/components/LandingPage/HeroBanner.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "./HeroBanner.css";
+import heroImage from "./SK_Hero.webp";
 
 const HeroBanner = () => {
   return (
-    <div className="hero-banner">
-      <div className="hero-text">
-        <span>S</span>
-        <span>I</span>
-        <span>D</span>
-        <span>N</span>
-        <span>E</span>
-        <span>Y</span>
-        <span> </span>
-        <span>K</span>
-        <span>A</span>
-        <span>H</span>
-        <span>A</span>
-        <span>N</span>
+    <div
+      className="hero-banner"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    >
+      <div className="hero-overlay">
+        <div className="hero-text">
+          <span>S</span>
+          <span>I</span>
+          <span>D</span>
+          <span>N</span>
+          <span>E</span>
+          <span>Y</span>
+          <span> </span>
+          <span>K</span>
+          <span>A</span>
+          <span>H</span>
+          <span>A</span>
+          <span>N</span>
+        </div>
       </div>
     </div>
   );
